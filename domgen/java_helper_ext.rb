303ec6922aa0ea7c8668a3456903773a86fcb95f
Peter Donald <peter@realityforge.org>
2010-05-04 23:56:56 +0000

    Make sure attributes are accessed through getters

--- domgen/java_helper_ext.rb
+++ domgen/java_helper_ext.rb
@@ -1,11 +1,19 @@
+def java_getter_name(name)
+  "get#{name}"
+end
+
+def java_setter_name(name)
+  "set#{name}()"
+end
+
 def java_accessors(name,type)
   <<JAVA
-  public #{type} get#{name}()
+  public #{type} #{java_getter_name(name)}()
   {
      return #{name};
   }
 
-  public void set#{name}( final #{type} value )
+  public void #{java_setter_name(name)}( final #{type} value )
   {
      #{name} = value;
   }
