7ed2ede6ca66e5979736b997fb8f9fd09e6153af
Peter Donald <peter@realityforge.org>
2010-05-04 05:12:49 +0000

    Merge all the SQL DDL generation into one template

--- domgen/sql_generator_ext.rb
+++ domgen/sql_generator_ext.rb
@@ -2,7 +2,6 @@ module Domgen
   module Generator
     def self.define_sql_templates(template_set)
       template_set.per_schema << Template.new('sql/ddl', 'schema.sql', 'databases/#{schema.name}')
-      template_set.per_schema << Template.new('sql/constraints', '#{schema.name}_constraints.sql', 'databases/#{schema.name}')
     end
   end
 end
