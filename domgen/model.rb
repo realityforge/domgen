64258010e5905859e2c102e435a2f3bbf6317a18
Peter Donald <peter@realityforge.org>
2010-05-04 04:25:57 +0000

    Move the generator and model code into separate files

---
+++ domgen/model.rb
@@ -0,0 +1,285 @@
+module Domgen
+  class BaseConfigElement
+    def initialize(options = {})
+      options.each_pair do |k, v|
+        self.send "#{k}=", v
+      end
+      yield self if block_given?
+      extension_point(:post_create)
+    end
+
+    @@extensions = {}
+    def self.extensions
+      @@extensions[self.name] ||= []
+    end
+
+    def extension_point(action)
+      self.class.extensions.each do |extension|
+        extension_object = (self.send extension rescue nil)
+        if extension_object && extension_object.respond_to?(action)
+          extension_object.send action
+        end
+      end
+    end
+  end
+
+  class AttributeSetConstraint < BaseConfigElement
+    attr_reader :name
+    attr_accessor :attribute_names
+
+    def initialize(name, attribute_names, options, &block)
+      @name, @attribute_names = name, attribute_names
+      super(options, &block)
+    end
+  end
+
+  class Attribute < BaseConfigElement
+    attr_reader :object_type
+    attr_reader :name
+    attr_reader :attribute_type
+
+    def initialize(object_type, name, attribute_type, options = {}, &block)
+      @object_type = object_type
+      @name = name
+      @attribute_type = attribute_type
+      super(options, &block)
+      raise "Invalid type #{attribute_type} for persistent attribute #{name}" if persistent? && !self.class.persistent_types.include?(attribute_type)
+    end
+
+    def reference?
+      self.attribute_type == :reference
+    end
+
+    attr_writer :validate
+
+    def validate?
+      @validate = true if @validate.nil?
+      @validate
+    end
+
+    attr_writer :unique
+
+    def unique?
+      @unique = false if @unique.nil?
+      @unique
+    end
+
+    attr_writer :primary_key
+
+    def primary_key?
+      @primary_key = false if @primary_key.nil?
+      @primary_key
+    end
+
+    attr_reader :length
+
+    def length=(length)
+      raise "length on #{name} is invalid as attribute is not a string" unless self.attribute_type == :string
+      @length = length
+    end
+
+    attr_writer :unique
+
+    def unique?
+      @unique = false if @unique.nil?
+      @unique
+    end
+
+    attr_writer :nullable
+
+    def nullable?
+      @nullable = false if @nullable.nil?
+      @nullable
+    end
+
+    attr_reader :values
+
+    def values=(values)
+      raise "values on #{name} is invalid as attribute is not an i_enum" unless self.attribute_type == :i_enum
+      @values = values
+    end
+
+    attr_writer :immutable
+
+    def immutable?
+      @immutable = false if @immutable.nil?
+      @immutable
+    end
+
+    attr_writer :persistent
+
+    def persistent?
+      @persistent = true if @persistent.nil?
+      @persistent
+    end
+
+    attr_reader :references
+
+    def references=(references)
+      raise "references on #{name} is invalid as attribute is not a reference" unless reference?
+      @references = references
+    end
+
+    def referenced_object
+      raise "referenced_object on #{name} is invalid as attribute is not a reference" unless reference?
+      self.object_type.schema.object_type_by_name(self.references)
+    end
+
+    attr_writer :inverse_relationship_type
+
+    def inverse_relationship_type
+      raise "inverse_relationship_type on #{name} is invalid as attribute is not a reference" unless reference?
+      @inverse_relationship_type = :has_many if @inverse_relationship_type.nil?
+      @inverse_relationship_type
+    end
+
+    attr_writer :inverse_relationship_name
+
+    def inverse_relationship_name
+      raise "inverse_relationship_name on #{name} is invalid as attribute is not a reference" unless reference?
+      @inverse_relationship_name = object_type.name if @inverse_relationship_name.nil?
+      @inverse_relationship_name
+    end
+
+    def self.persistent_types
+      [:text, :string, :reference, :boolean, :integer, :i_enum]
+    end
+  end
+
+  class ObjectType < BaseConfigElement
+    attr_reader :schema
+    attr_reader :name
+    attr_reader :attributes
+    attr_reader :validations
+    attr_reader :queries
+    attr_reader :codependent_constraints
+    attr_reader :incompatible_constraints
+    attr_reader :referencing_attributes
+
+    def initialize(schema, name, options = {}, &block)
+      @schema, @name = schema, name
+      @attributes = []
+      @validations = []
+      @codependent_constraints = []
+      @incompatible_constraints = []
+      @queries = []
+      @referencing_attributes = []
+      super(options, &block)
+    end
+
+    def object_type
+      self
+    end
+
+    def boolean(name, options = {}, &block)
+      attribute(name, :boolean, options, &block)
+    end
+
+    def text(name, options = {}, &block)
+      attribute(name, :text, options, &block)
+    end
+
+    def string(name, length, options = {}, &block)
+      attribute(name, :string, options.merge({:length => length}), &block)
+    end
+
+    def integer(name, options = {}, &block)
+      attribute(name, :integer, options, &block)
+    end
+
+    def reference(other_type, options = {}, &block)
+      name = (options.delete(:name) || other_type).to_s.to_sym
+      attribute(name, :reference, options.merge({:references => other_type}), &block)
+    end
+
+    def i_enum(name, values, options = {}, &block)
+      values.each_pair do |k, v|
+        raise "Key #{k} of i_enum #{name} should be a string" unless k.instance_of?(String)
+        raise "Value #{v} for key #{k} of i_enum #{name} should be an integer" unless v.instance_of?(Fixnum)
+      end
+      attribute(name, :i_enum, options.merge({:values => values}), &block)
+    end
+
+    def attribute(name, type, options = {}, &block)
+      attribute = Attribute.new(self, name, type, options, &block)
+      @attributes << attribute
+      attribute
+    end
+
+    def codependent_constraint(name, attribute_names, options = {}, &block)
+      codependent_constraint = AttributeSetConstraint.new(name, attribute_names, options, &block)
+      @codependent_constraints << codependent_constraint
+      codependent_constraint
+    end
+
+    def incompatible_constraint(name, attribute_names, options = {}, &block)
+      incompatible_constraint = AttributeSetConstraint.new(name, attribute_names, options, &block)
+      @incompatible_constraints << incompatible_constraint
+      incompatible_constraint
+    end
+
+    # Assume single column pk
+    def primary_key
+      attributes.find {|a| a.primary_key? }
+    end
+
+    def attribute_by_name(name)
+      attribute = attributes.find{|a| a.name.to_s == name.to_s}
+      raise "Unable to find attribute named #{name} on type #{self.name}" unless attribute
+      attribute
+    end
+  end
+
+  class Schema < BaseConfigElement
+    attr_reader :schema_set
+    attr_reader :name
+    attr_reader :object_types
+
+    def initialize(schema_set, name, options = {}, &block)
+      @schema_set = schema_set
+      @name = name
+      @object_types = []
+      super(options, &block)
+    end
+
+    def schema
+      self
+    end
+
+    def define_object_type(name, options = {}, &block)
+      @object_types << ObjectType.new(self, name, options, &block)
+    end
+
+    def object_type_by_name(name)
+      object_type = @object_types.find{|o|o.name.to_s == name.to_s}
+      raise "Unable to locate object_type #{name}" unless object_type
+      object_type
+    end
+  end
+
+  class SchemaSet < BaseConfigElement
+    attr_reader :schemas
+
+    def initialize(options = {}, &block)
+      @schemas = []
+      super(options, &block)
+      self.schemas.each do |schema|
+        schema.object_types.each do |object_type|
+          object_type.attributes.each do |attribute|
+            if attribute.reference?
+              attribute.referenced_object.referencing_attributes << attribute
+            end
+          end
+        end
+      end
+    end
+
+    def schema_set
+      self
+    end
+
+    def define_schema(name, options = {}, &block)
+      @schemas << Domgen::Schema.new(self, name, options, &block)
+    end
+  end
+end
