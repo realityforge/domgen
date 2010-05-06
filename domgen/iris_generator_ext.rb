55d17848d4ffc6248e42ca95a391e298ad307595
Peter Donald <peter@realityforge.org>
2010-05-06 07:16:09 +0000

    Start generating other iris required artifacts

--- domgen/iris_generator_ext.rb
+++ domgen/iris_generator_ext.rb
@@ -4,6 +4,21 @@ module Domgen
       template_set.per_object_type << Template.new('iris/model',
                                                    '#{object_type.java.fully_qualified_name.gsub(".","/")}Bean.java',
                                                    'iris')
+      template_set.per_schema << Template.new('iris/visitor',
+                                              '#{schema.java.package.gsub(".","/")}/visitor/Visitor.java',
+                                              'iris')
+      template_set.per_schema << Template.new('iris/abstract_visitor',
+                                              '#{schema.java.package.gsub(".","/")}/visitor/AbstractVisitor.java',
+                                              'iris')
+      template_set.per_schema << Template.new('iris/filter',
+                                              '#{schema.java.package.gsub(".","/")}/visitor/Filter.java',
+                                              'iris')
+      template_set.per_schema << Template.new('iris/abstract_filter',
+                                              '#{schema.java.package.gsub(".","/")}/visitor/AbstractFilter.java',
+                                              'iris')
+      template_set.per_schema << Template.new('iris/chain_filter',
+                                              '#{schema.java.package.gsub(".","/")}/visitor/ChainFilter.java',
+                                              'iris')
     end
   end
 end
