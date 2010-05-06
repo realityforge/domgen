d5ce2bf118ac4c494eeb24966c583bfcf01569eb
Peter Donald <peter@realityforge.org>
2010-05-06 00:18:19 +0000

    Generate iris stuff into a separate dir

--- domgen/iris_generator_ext.rb
+++ domgen/iris_generator_ext.rb
@@ -3,7 +3,7 @@ module Domgen
     def self.define_iris_templates(template_set)
       template_set.per_object_type << Template.new('iris/model',
                                                    '#{object_type.java.fully_qualified_name.gsub(".","/")}Bean.java',
-                                                   'java')
+                                                   'iris')
     end
   end
 end
