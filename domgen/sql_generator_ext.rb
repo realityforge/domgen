1fddf892de7e693ef2098d2f09fe57169e3a3d39
Peter Donald <peter@realityforge.org>
2010-05-10 01:42:09 +0000

    Remove domgen dir in preparation to moving to externals

--- domgen/sql_generator_ext.rb
+++
@@ -1,7 +0,0 @@
-module Domgen
-  module Generator
-    def self.define_sql_templates(template_set)
-      template_set.per_schema << Template.new('sql/ddl', 'schema.sql', 'databases/#{schema.name}')
-    end
-  end
-end
