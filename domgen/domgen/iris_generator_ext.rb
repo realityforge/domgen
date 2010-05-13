476ed0cb3355bf85d8ebbe09ef46f6f4e983e298
Peter Donald <peter@realityforge.org>
2010-05-13 06:38:53 +0000

    Start to generate a class that will provide functionality previously achieved by inspecting object descriuptors

--- domgen/domgen/iris_generator_ext.rb
+++ domgen/domgen/iris_generator_ext.rb
@@ -1,6 +1,10 @@
 module Domgen
   module Generator
     def self.define_iris_templates(template_set)
+      template_set.per_schema << Template.new('iris/module',
+                                              '#{schema.java.package.gsub(".","/")}/#{schema.name}Module.java',
+                                              'iris',
+                                              'schema.iris.generate?')
       template_set.per_object_type << Template.new('iris/model',
                                                    '#{object_type.java.fully_qualified_name.gsub(".","/")}Bean.java',
                                                    'iris',
