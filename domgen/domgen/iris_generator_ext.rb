d42dec919ed8094409b5e709f5415b13f2abd1e7
Peter Donald <peter@realityforge.org>
2010-05-14 01:49:27 +0000

    Generate to src dir to match existing iris generator

--- domgen/domgen/iris_generator_ext.rb
+++ domgen/domgen/iris_generator_ext.rb
@@ -3,35 +3,35 @@ module Domgen
     def self.define_iris_templates(template_set)
       template_set.per_schema << Template.new('iris/module',
                                               '#{schema.java.package.gsub(".","/")}/#{schema.name}Module.java',
-                                              'iris',
+                                              'src',
                                               'schema.iris.generate?')
       template_set.per_object_type << Template.new('iris/model',
                                                    '#{object_type.java.fully_qualified_name.gsub(".","/")}Bean.java',
-                                                   'iris',
+                                                   'src',
                                                    'object_type.iris.generate?')
       template_set.per_schema << Template.new('iris/visitor',
                                               '#{schema.java.package.gsub(".","/")}/visitor/Visitor.java',
-                                              'iris',
+                                              'src',
                                               'schema.iris.generate?')
       template_set.per_schema << Template.new('iris/abstract_visitor',
                                               '#{schema.java.package.gsub(".","/")}/visitor/AbstractVisitor.java',
-                                              'iris',
+                                              'src',
                                               'schema.iris.generate?')
       template_set.per_schema << Template.new('iris/filter',
                                               '#{schema.java.package.gsub(".","/")}/visitor/Filter.java',
-                                              'iris',
+                                              'src',
                                               'schema.iris.generate?')
       template_set.per_schema << Template.new('iris/abstract_filter',
                                               '#{schema.java.package.gsub(".","/")}/visitor/AbstractFilter.java',
-                                              'iris',
+                                              'src',
                                               'schema.iris.generate?')
       template_set.per_schema << Template.new('iris/chain_filter',
                                               '#{schema.java.package.gsub(".","/")}/visitor/ChainFilter.java',
-                                              'iris',
+                                              'src',
                                               'schema.iris.generate?')
       template_set.per_schema << Template.new('iris/traverser',
                                               '#{schema.java.package.gsub(".","/")}/visitor/Traverser.java',
-                                              'iris',
+                                              'src',
                                               'schema.iris.generate?')
     end
   end
