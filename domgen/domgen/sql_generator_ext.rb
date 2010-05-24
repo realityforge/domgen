bb27f5872af6700df02c036a433587587b078811
Peter Donald <peter@realityforge.org>
2010-05-24 03:19:03 +0000

    Generate schema into lowercased dir name to match existing conventions

--- domgen/domgen/sql_generator_ext.rb
+++ domgen/domgen/sql_generator_ext.rb
@@ -1,7 +1,7 @@
 module Domgen
   module Generator
     def self.define_sql_templates(template_set)
-      template_set.per_schema << Template.new('sql/ddl', 'schema.sql', 'databases/#{schema.name}')
+      template_set.per_schema << Template.new('sql/ddl', 'schema.sql', '#{underscore(schema.name)}')
     end
   end
 end
