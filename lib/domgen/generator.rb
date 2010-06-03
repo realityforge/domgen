dec1702bed2bdf732b1e48ec59b403bfb08f0250
Peter Donald <peter@realityforge.org>
2010-06-03 23:17:05 +1000

    Begin to refactor the generator to separate the render context, the template definition and the action of generating files

--- lib/domgen/generator.rb
+++ lib/domgen/generator.rb
@@ -20,23 +20,29 @@ module Domgen
         end
       end
 
-      template_set.per_schema_set.each do |template_map|
-        Logger.debug "Generator: #{template_map.template_name} => #{template_map.basedir} for schema set"
-        template_map.generate(directory, schema_set)
+      template_set.per_schema_set.each do |template|
+        context = RenderContext.new
+        context.set_local_variable(:schema_set, schema_set)
+        output_filename = render(directory, template, context)
+        Logger.debug "Generated #{template.template_name} for schema set" if output_filename
       end
 
-      template_set.per_schema.each do |template_map|
+      template_set.per_schema.each do |template|
+        context = RenderContext.new
         schema_set.schemas.each do |schema|
-          Logger.debug "Generator: #{template_map.template_name} => #{template_map.basedir} for schema #{schema.name}"
-          template_map.generate(directory, schema)
+          context.set_local_variable(:schema, schema)
+          output_filename = render(directory, template, context)
+          Logger.debug "Generated #{template.template_name} for schema #{schema.name}" if output_filename
         end
       end
 
-      template_set.per_object_type.each do |template_map|
+      template_set.per_object_type.each do |template|
+        context = RenderContext.new
         schema_set.schemas.each do |schema|
           schema.object_types.each do |object_type|
-            Logger.debug "Generator: #{template_map.template_name} => #{template_map.basedir} for object_type #{object_type.name} in schema #{schema.name}"
-            template_map.generate(directory, object_type)
+            context.set_local_variable(:object_type, object_type)
+            output_filename = render(directory, template, context)
+            Logger.debug "Generated #{template.template_name} for object_type #{schema.name}.#{object_type.name}" if output_filename
           end
         end
       end
@@ -56,35 +62,81 @@ module Domgen
     end
 
     class Template
+      class << self
+        attr_accessor :template_dir
+      end
+
+      self.template_dir = "#{File.dirname(__FILE__)}/templates"
+
+
       attr_reader :template_name
+      attr_reader :template_type
       attr_reader :output_filename_pattern
       attr_reader :basedir
       attr_reader :guard
 
-      def initialize(template_name, output_filename_pattern, basedir, guard = nil)
-        @template_name, @output_filename_pattern, @basedir, @guard = template_name, output_filename_pattern, basedir, guard
+      def initialize(template_name, output_filename_pattern, basedir, guard = nil, template_type = :erb)
+        @template_name, @output_filename_pattern, @basedir, @guard, @template_type =
+            template_name, output_filename_pattern, basedir, guard, template_type
       end
 
-      def generate(basedir, context)
-        context_binding = context.send :binding
-        return if !guard.nil? && !eval(guard, context_binding)
-        output_filename = eval("\"#{output_filename_pattern}\"", context_binding)
-        output_dir = eval("\"#{self.basedir}\"", context_binding)
-        output_filename = File.join(basedir, output_dir, output_filename)
-        result = erb_instance.result(context_binding)
-        FileUtils.mkdir_p File.dirname(output_filename)
-        File.open(output_filename, 'w') { |f| f.write(result) }
+      def render_to_string(context_binding)
+        return erb_instance.result(context_binding) if :erb == template_type
+        raise "Unknown template_type #{template_type}"
       end
 
       protected
 
       def erb_instance
         unless @template
-          filename = "#{File.dirname(__FILE__)}/templates/#{template_name}.erb"
+          filename = "#{self.class.template_dir}/#{template_name}.erb"
           @template = ERB.new(IO.read(filename))
         end
         @template
       end
+
+    end
+
+    class RenderContext
+      def initialize
+        @helpers = []
+        @variables = {}
+      end
+
+      def add_helper(module_type)
+        @helpers << module_type
+      end
+
+      def set_local_variable(key, value)
+        @variables[key] = value
+      end
+
+      def context
+        context = Object.new
+        @helpers.each do |helper|
+          context.include helper
+        end
+        @variables.each_pair do |k, v|
+          context.instance_eval "def #{k}; @#{k}; end"
+          context.instance_variable_set "@#{k}".to_sym, v
+        end
+
+        context
+      end
+    end
+    
+    private
+
+    def self.render(target_basedir, template, render_context)
+      context_binding = render_context.context.send :binding
+      return nil if !template.guard.nil? && !eval(template.guard, context_binding)
+      output_filename = eval("\"#{template.output_filename_pattern}\"", context_binding)
+      output_dir = eval("\"#{template.basedir}\"", context_binding)
+      output_filename = File.join(target_basedir, output_dir, output_filename)
+      result = template.render_to_string(context_binding)
+      FileUtils.mkdir_p File.dirname(output_filename)
+      File.open(output_filename, 'w') { |f| f.write(result) }
+      return output_filename
     end
   end
 end
