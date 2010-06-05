7ff8b1e56c9178b24e4583fa616b54105346a5c5
Peter Donald <peter@realityforge.org>
2010-06-05 16:40:14 +1000

    Update iris generator to use a helper class

--- lib/domgen/iris_generator_ext.rb
+++ lib/domgen/iris_generator_ext.rb
@@ -3,26 +3,28 @@ module Domgen
     def self.define_iris_templates
       schema_file_prefix = 'java/#{schema.java.package.gsub(".","/")}/#{schema.name}'
       visitor_pkg_prefix = 'java/#{schema.java.package.gsub(".","/")}/visitor/'
+      helpers = [IrisHelper]
       [
           Template.new(:object_type,
                        'iris/model',
-                       'java/#{object_type.java.fully_qualified_name.gsub(".","/")}Bean.java'),
+                       'java/#{object_type.java.fully_qualified_name.gsub(".","/")}Bean.java',
+                       helpers),
           Template.new(:object_type,
                        'iris/persist_peer',
                        'java/#{object_type.schema.java.package.gsub(".","/")}/persist/#{object_type.java.classname}PersistPeer.java',
-                       [],
+                       helpers,
                        'object_type.name != :Batch'),
-          Template.new(:schema, 'iris/xml_generator', "#{schema_file_prefix}XMLGenerator.java"),
-          Template.new(:schema, 'iris/sync', "#{schema_file_prefix}Sync.java"),
-          Template.new(:schema, 'iris/codec', "#{schema_file_prefix}Codec.java"),
-          Template.new(:schema, 'iris/module', "#{schema_file_prefix}Module.java"),
-          Template.new(:schema, 'iris/validator', "#{schema_file_prefix}Validator.java"),
-          Template.new(:schema, 'iris/visitor', "#{visitor_pkg_prefix}Visitor.java"),
-          Template.new(:schema, 'iris/abstract_visitor', "#{visitor_pkg_prefix}AbstractVisitor.java"),
-          Template.new(:schema, 'iris/filter', "#{visitor_pkg_prefix}Filter.java"),
-          Template.new(:schema, 'iris/abstract_filter', "#{visitor_pkg_prefix}AbstractFilter.java"),
-          Template.new(:schema, 'iris/chain_filter', "#{visitor_pkg_prefix}ChainFilter.java"),
-          Template.new(:schema, 'iris/traverser', "#{visitor_pkg_prefix}Traverser.java"),
+          Template.new(:schema, 'iris/xml_generator', "#{schema_file_prefix}XMLGenerator.java", helpers),
+          Template.new(:schema, 'iris/sync', "#{schema_file_prefix}Sync.java", helpers),
+          Template.new(:schema, 'iris/codec', "#{schema_file_prefix}Codec.java", helpers),
+          Template.new(:schema, 'iris/module', "#{schema_file_prefix}Module.java", helpers),
+          Template.new(:schema, 'iris/validator', "#{schema_file_prefix}Validator.java", helpers),
+          Template.new(:schema, 'iris/visitor', "#{visitor_pkg_prefix}Visitor.java", helpers),
+          Template.new(:schema, 'iris/abstract_visitor', "#{visitor_pkg_prefix}AbstractVisitor.java", helpers),
+          Template.new(:schema, 'iris/filter', "#{visitor_pkg_prefix}Filter.java", helpers),
+          Template.new(:schema, 'iris/abstract_filter', "#{visitor_pkg_prefix}AbstractFilter.java", helpers),
+          Template.new(:schema, 'iris/chain_filter', "#{visitor_pkg_prefix}ChainFilter.java", helpers),
+          Template.new(:schema, 'iris/traverser', "#{visitor_pkg_prefix}Traverser.java", helpers),
       ]
     end
   end
--- lib/domgen/iris_helper_ext.rb
+++ lib/domgen/iris_helper_ext.rb
@@ -1,57 +1,58 @@
-def iris_immutable_check(name)
-  <<JAVA
+module IrisHelper
+  def iris_immutable_check(name)
+    <<JAVA
     if( !isNew() && !isLoading() )
     {
        throw new IllegalStateException( "Attempting to modify immutable attribute #{name} on non-new object" );
     }
 JAVA
-end
+  end
 
-def iris_nullable_check(name)
-  <<JAVA
+  def iris_nullable_check(name)
+    <<JAVA
       if( !isUnloading() && null == value )
       {
          throw new IllegalStateException( "Attempting to null non-nullable attribute #{name}" );
       }
 JAVA
-end
+  end
 
-def iris_to_string_methods(object_type)
-  return '' if object_type.abstract?
-  s = <<JAVA
+  def iris_to_string_methods(object_type)
+    return '' if object_type.abstract?
+    s = <<JAVA
   public String toDebugString()
   {
     return "#{object_type.name}[" +
 JAVA
-  s += object_type.java.debug_attributes.collect do |a|
-    attr = object_type.attribute_by_name(a)
-    accessor = attr.reference? ? attr.referencing_link_name : attr.java.field_name
-    "           \"#{accessor} = \" + get#{accessor}()"
-  end.join(" + \", \" +\n")
+    s += object_type.java.debug_attributes.collect do |a|
+      attr = object_type.attribute_by_name(a)
+      accessor = attr.reference? ? attr.referencing_link_name : attr.java.field_name
+      "           \"#{accessor} = \" + get#{accessor}()"
+    end.join(" + \", \" +\n")
 
-  s += <<JAVA
+    s += <<JAVA
  +
            "]";
   }
 JAVA
-  s += <<JAVA
+    s += <<JAVA
   public String toLabel()
   {
 JAVA
 
-  if !object_type.java.label_attribute.nil?
-    label = object_type.attribute_by_name(object_type.java.label_attribute)
-    s += <<JAVA
+    if !object_type.java.label_attribute.nil?
+      label = object_type.attribute_by_name(object_type.java.label_attribute)
+      s += <<JAVA
     return String.valueOf( get#{label.java.field_name}() );
 JAVA
-  else
-    pk = object_type.primary_key
-    s += <<JAVA
+    else
+      pk = object_type.primary_key
+      s += <<JAVA
     return "#{object_type.name}[#{pk.java.field_name} = " + get#{pk.java.field_name}() +"]";
 JAVA
-  end
+    end
 
-  s += <<JAVA
+    s += <<JAVA
   }
 
   @Override
@@ -60,4 +61,6 @@ JAVA
     return #{!object_type.java.label_attribute.nil? ? 'toLabel' : 'toDebugString'}();
   }
 JAVA
+  end
 end
+
