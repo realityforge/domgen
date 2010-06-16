9571b019f108f96c383f819af15b5edf13c8d733
Peter Donald <peter@realityforge.org>
2010-06-16 17:26:24 +1000

    Move all jpa artifacts into a separate dir

--- lib/domgen/domgen.rb
+++ lib/domgen/domgen.rb
@@ -15,17 +15,19 @@ require "#{File.dirname(__FILE__)}/helper.rb"
 require "#{File.dirname(__FILE__)}/java_model_ext.rb"
 require "#{File.dirname(__FILE__)}/ruby_model_ext.rb"
 require "#{File.dirname(__FILE__)}/sql_model_ext.rb"
-require "#{File.dirname(__FILE__)}/jpa_model_ext.rb"
 require "#{File.dirname(__FILE__)}/iris_model_ext.rb"
 
 # Helper extensions
-require "#{File.dirname(__FILE__)}/jpa_helper_ext.rb"
 require "#{File.dirname(__FILE__)}/sql_helper_ext.rb"
 require "#{File.dirname(__FILE__)}/iris_helper_ext.rb"
 
 # Generator extensions
-require "#{File.dirname(__FILE__)}/jpa_generator_ext.rb"
 require "#{File.dirname(__FILE__)}/sql_generator_ext.rb"
 require "#{File.dirname(__FILE__)}/active_record_generator_ext.rb"
 require "#{File.dirname(__FILE__)}/iris_generator_ext.rb"
 require "#{File.dirname(__FILE__)}/iris_sql_generator_ext.rb"
+
+# JPA
+require "#{File.dirname(__FILE__)}/jpa/jpa_model_ext.rb"
+require "#{File.dirname(__FILE__)}/jpa/jpa_helper_ext.rb"
+require "#{File.dirname(__FILE__)}/jpa/jpa_generator_ext.rb"
