008e61c0383756a571a38400d6d5ec5b490aa3c9
Peter Donald <peter@realityforge.org>
2010-06-23 10:04:14 +1000

    Generate sql into dir that matches schema name

--- lib/domgen/sql/generator.rb
+++ lib/domgen/sql/generator.rb
@@ -3,7 +3,7 @@ module Domgen
     def self.define_sql_templates
       template_dir = "#{File.dirname(__FILE__)}/templates"
       [
-          Template.new(:schema, "#{template_dir}/ddl.erb", '#{underscore(schema.name)}/schema.sql'),
+          Template.new(:schema, "#{template_dir}/ddl.erb", '#{schema.name}/schema.sql'),
       ]
     end
   end
