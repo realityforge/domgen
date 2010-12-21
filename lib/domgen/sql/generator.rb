d4ec1ce46dece8e0d9389c9f1bb805dc2bfdd59a
Peter Donald <peter@realityforge.org>
2010-12-22 10:13:06 +1100

    Split the creation of triggers/constraints and the enabling of triggers constraints into two separate phases

--- lib/domgen/sql/generator.rb
+++ lib/domgen/sql/generator.rb
@@ -4,7 +4,8 @@ module Domgen
       template_dir = "#{File.dirname(__FILE__)}/templates"
       [
         Template.new(:data_module, "#{template_dir}/ddl.erb", '#{data_module.name}/schema.sql'),
-        Template.new(:data_module, "#{template_dir}/constraints.erb", '#{data_module.name}/finalize/constraints.sql')
+        Template.new(:data_module, "#{template_dir}/constraints.erb", '#{data_module.name}/schema_constraints.sql'),
+        Template.new(:data_module, "#{template_dir}/constraints_check.erb", '#{data_module.name}/finalize/check_constraints.sql'),
       ]
     end
   end
