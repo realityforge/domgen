38b32f508e7a4c3043e76cbe51cd1940d56df26a
Peter Donald <peter@realityforge.org>
2011-01-10 16:56:26 +1100

    Indent the files

--- lib/domgen/xml/helper.rb
+++ lib/domgen/xml/helper.rb
@@ -1,138 +1,141 @@
-module Domgen::Xml
-  module Helper
-    def to_tag_name(name)
-      name.to_s.gsub(/_/, '-').gsub(/\?/, '')
-    end
-
-    def tag_each(target, name)
-      values = target.send(name)
-      unless values.nil? || values.empty?
-        doc.tag!(to_tag_name(name)) do
-          values.each do |item|
-            yield item
-          end
-        end
-      end
-    end
-
-    def collect_attributes(target, names)
-      attributes = Hash.new
-      names.each do |name|
-        value = target.send(name.to_sym)
-        if value
-          attributes[to_tag_name(name)] = value
-        end
-      end
-      attributes
-    end
-
-    def format_text(text)
-      p = Pushdown.new
-      text.lines.each do |line|
-        p.add_line(line)
-      end
-      p.write(doc)
-    end
-
-    class Pushdown
-      ITEM = "listitem"
-      LIST = "itemizedlist"
-      PARA = "para"
-
-      def initialize
-        @levels = [[]]
-        @blank = false
-        push(PARA, 0)
+module Domgen
+  module Xml
+    module Helper
+      def to_tag_name(name)
+        name.to_s.gsub(/_/, '-').gsub(/\?/, '')
       end
 
-      def write(doc)
-        @levels[0].each do |lazy_tag|
-          lazy_tag.to_tag(doc)
-        end
-      end
-
-      def add_line(line)
-        if line.gsub(/\s/, '').empty?
-          # blank - end of paragraph
-          @blank = true
-        else
-          indent = indent_of(line)
-          while @levels.size > 1 && indent < @levels[-1].level
-            pop
-          end
-
-          if leading(line) == "*"
-            if @levels[-1].name == ITEM
-              # new listitem in an existing list
-              pop; push(ITEM, indent); push(PARA, indent + 2)
-            else
-              # new list
-              pop; push(LIST, indent); push(ITEM, indent); push(PARA, indent + 2)
+      def tag_each(target, name)
+        values = target.send(name)
+        unless values.nil? || values.empty?
+          doc.tag!(to_tag_name(name)) do
+            values.each do |item|
+              yield item
             end
-          elsif @levels[-1].name == ITEM
-            # close list item and list and open a paragraph
-            pop; pop; push(PARA, indent)
-          elsif @blank
-            # new paragraph
-            pop; push(PARA, indent)
           end
-          @levels[-1] << Text.new(line.gsub(/^\s*(\* )?/, ''))
+        end
+      end
+
+      def collect_attributes(target, names)
+        attributes = Hash.new
+        names.each do |name|
+          value = target.send(name.to_sym)
+          if value
+            attributes[to_tag_name(name)] = value
+          end
+        end
+        attributes
+      end
+
+      def format_text(text)
+        p = Pushdown.new
+        text.lines.each do |line|
+          p.add_line(line)
+        end
+        p.write(doc)
+      end
+
+      class Pushdown
+        ITEM = "listitem"
+        LIST = "itemizedlist"
+        PARA = "para"
+
+        def initialize
+          @levels = [[]]
           @blank = false
+          push(PARA, 0)
+        end
+
+        def write(doc)
+          @levels[0].each do |lazy_tag|
+            lazy_tag.to_tag(doc)
+          end
+        end
+
+        def add_line(line)
+          if line.gsub(/\s/, '').empty?
+            # blank - end of paragraph
+            @blank = true
+          else
+            indent = indent_of(line)
+            while @levels.size > 1 && indent < @levels[-1].level
+              pop
+            end
+
+            if leading(line) == "*"
+              if @levels[-1].name == ITEM
+                # new listitem in an existing list
+                pop; push(ITEM, indent); push(PARA, indent + 2)
+              else
+                # new list
+                pop; push(LIST, indent); push(ITEM, indent); push(PARA, indent + 2)
+              end
+            elsif @levels[-1].name == ITEM
+              # close list item and list and open a paragraph
+              pop; pop; push(PARA, indent)
+            elsif @blank
+              # new paragraph
+              pop; push(PARA, indent)
+            end
+            @levels[-1] << Text.new(line.gsub(/^\s*(\* )?/, ''))
+            @blank = false
+          end
+        end
+
+        private
+
+        def indent_of(line)
+          m = /^\s+/.match(line)
+          m.nil? ? 0 : m[0].size
+        end
+
+        def leading(line)
+          line.sub(/\s*/, '')[0..0]
+        end
+
+        def pop
+          @levels.pop unless 1 == @levels.size
+        end
+
+        def push(name, level)
+          tag = LazyTag.new(name, level)
+          @levels << tag
+          @levels[-2] << tag
         end
       end
 
-      private
+      class LazyTag
+        attr_accessor :level
+        attr_accessor :name
 
-      def indent_of(line)
-        m = /^\s+/.match(line)
-        m.nil? ? 0 : m[0].size
-      end
+        def initialize(name, level)
+          @name = name
+          @level = level
+          @children = []
+        end
 
-      def leading(line)
-        line.sub(/\s*/, '')[0..0]
-      end
+        def to_tag(doc)
+          doc.tag!(@name) do
+            @children.each { |child| child.to_tag(doc) }
+          end
+        end
 
-      def pop
-        @levels.pop unless 1 == @levels.size
-      end
-
-      def push(name, level)
-        tag = LazyTag.new(name, level)
-        @levels << tag
-        @levels[-2] << tag
-      end
-    end
-
-    class LazyTag
-      attr_accessor :level
-      attr_accessor :name
-      def initialize(name, level)
-        @name = name
-        @level = level
-        @children = []
-      end
-
-      def to_tag(doc)
-        doc.tag!(@name) do
-          @children.each {|child| child.to_tag(doc)}
+        def <<(child)
+          @children << child
         end
       end
 
-      def <<(child)
-        @children << child
+      class Text
+        def initialize(text)
+          @text = text
+        end
+
+        def to_tag(doc)
+          doc.text! @text
+        end
       end
+
     end
-
-    class Text
-      def initialize(text)
-        @text = text
-      end
-
-      def to_tag(doc)
-        doc.text! @text
-      end
-    end
-
   end
 end
 
--- lib/domgen/xml/templates/xml.rb
+++ lib/domgen/xml/templates/xml.rb
@@ -1,189 +1,191 @@
-module Domgen::Xml
-  module Templates
+module Domgen
   module Xml
-    def generate
-      @doc = Builder::XmlMarkup.new(:indent => 2)
+    module Templates
+      module Xml
+        def generate
+          @doc = Builder::XmlMarkup.new(:indent => 2)
 
-      visit_repository(@repository)
-    end
-
-    private
-    
-    attr_reader :doc
-
-    def visit_repository(repository)
-      doc.tag!("repository", :name => repository.name) do
-        add_tags(repository)
-        repository.data_modules.each do |data_module|
-          visit_data_module(data_module)
-        end
-      end
-    end
-
-    def visit_data_module(data_module)
-      doc.tag!("data-module", :name => data_module.name) do
-        add_tags(data_module)
-        data_module.object_types.each do |object_type|
-          visit_object_type(object_type)
-        end
-      end
-    end
-
-    def visit_object_type(object_type)
-      doc.tag!("object-type", collect_attributes(object_type, %w(name qualified_name))) do
-        add_tags(object_type)
-
-        tag_each(object_type, :attributes) do |attribute|
-          visit_attribute(attribute)
+          visit_repository(@repository)
         end
 
-        %w(unique codependent incompatible).each do |constraint_type|
-          tag_each(object_type, "#{constraint_type}_constraints".to_sym) do |constraint|
-            doc.tag!("#{constraint_type}-constraint") do
-              constraint.attribute_names.each do |name|
-                attribute_ref(object_type, name)
-              end
+        private
+
+        attr_reader :doc
+
+        def visit_repository(repository)
+          doc.tag!("repository", :name => repository.name) do
+            add_tags(repository)
+            repository.data_modules.each do |data_module|
+              visit_data_module(data_module)
             end
           end
         end
 
-        tag_each(object_type, :dependency_constraints) do |constraint|
-          doc.tag!("dependency-constraint") do
-            attribute_ref(object_type, constraint.attribute_name)
-            doc.tag!("dependent-attributes") do
-              constraint.dependent_attribute_names.each do |name|
-                attribute_ref(object_type, name)
-              end
+        def visit_data_module(data_module)
+          doc.tag!("data-module", :name => data_module.name) do
+            add_tags(data_module)
+            data_module.object_types.each do |object_type|
+              visit_object_type(object_type)
             end
           end
         end
 
-        tag_each(object_type, :cycle_constraints) do |constraint|
-          doc.tag!("cycle-constraint") do
-            attribute_ref(object_type, constraint.attribute_name)
-            doc.tag!("path") do
-              constraint.attribute_name_path.reduce object_type do |path_object_type, attribute_name|
-                attribute_ref(path_object_type, attribute_name)
-                path_object_type.attribute_by_name(attribute_name).referenced_object
+        def visit_object_type(object_type)
+          doc.tag!("object-type", collect_attributes(object_type, %w(  name qualified_name  ))) do
+            add_tags(object_type)
+
+            tag_each(object_type, :attributes) do |attribute|
+              visit_attribute(attribute)
+            end
+
+            %w(  unique codependent incompatible  ).each do |constraint_type|
+              tag_each(object_type, "#{constraint_type}_constraints".to_sym) do |constraint|
+                doc.tag!("#{constraint_type}-constraint") do
+                  constraint.attribute_names.each do |name|
+                    attribute_ref(object_type, name)
+                  end
+                end
               end
             end
-            doc.tag!("scoping-attribute") do
-              attribute_ref(object_type, constraint.scoping_attribute)
+
+            tag_each(object_type, :dependency_constraints) do |constraint|
+              doc.tag!("dependency-constraint") do
+                attribute_ref(object_type, constraint.attribute_name)
+                doc.tag!("dependent-attributes") do
+                  constraint.dependent_attribute_names.each do |name|
+                    attribute_ref(object_type, name)
+                  end
+                end
+              end
             end
+
+            tag_each(object_type, :cycle_constraints) do |constraint|
+              doc.tag!("cycle-constraint") do
+                attribute_ref(object_type, constraint.attribute_name)
+                doc.tag!("path") do
+                  constraint.attribute_name_path.reduce object_type do |path_object_type, attribute_name|
+                    attribute_ref(path_object_type, attribute_name)
+                    path_object_type.attribute_by_name(attribute_name).referenced_object
+                  end
+                end
+                doc.tag!("scoping-attribute") do
+                  attribute_ref(object_type, constraint.scoping_attribute)
+                end
+              end
+            end
+
+            visit_table(object_type.sql)
           end
         end
 
-        visit_table(object_type.sql)
-      end
-    end
-
-    def visit_attribute(attribute)
-      attribute_names = %w(abstract? override? reference? validate? set_once? generated_value?
+        def visit_attribute(attribute)
+          attribute_names = %w(  abstract? override? reference? validate? set_once? generated_value?
                            enum? primary_key? allow_blank? unique? nullable? immutable? persistent?
-                           updatable? allow_blank? qualified_name length min_length name)
-      doc.attribute({"object-type" => attribute.object_type.qualified_name},
-                    collect_attributes(attribute, attribute_names)) do
-        add_tags(attribute)
+                           updatable? allow_blank? qualified_name length min_length name  )
+          doc.attribute({"object-type" => attribute.object_type.qualified_name},
+                        collect_attributes(attribute, attribute_names)) do
+            add_tags(attribute)
 
-        unless attribute.values.nil?
-          doc.values do
-            attribute.values.each_pair do |name, value|
-              doc.value(:code => name, :value => value)
+            unless attribute.values.nil?
+              doc.values do
+                attribute.values.each_pair do |name, value|
+                  doc.value(:code => name, :value => value)
+                end
+              end
             end
+
+            if attribute.reference?
+              doc.reference("references" => attribute.references,
+                            "referenced-object" => attribute.referenced_object.qualified_name,
+                            "polymorphic" => attribute.polymorphic?.to_s,
+                            "link-name" => attribute.referencing_link_name,
+                            "inverse-multiplicity" => attribute.inverse_multiplicity.to_s,
+                            "inverse-traversable" => attribute.inverse_traversable?.to_s,
+                            "inverse-relationship" => attribute.inverse_relationship_name.to_s)
+            end
+
+            if attribute.persistent?
+              doc.persistent(collect_attributes(attribute.sql, %w(  column_name sql_type identity? sparse?
+                                                              calculation  )))
+            end
+
           end
         end
 
-        if attribute.reference?
-          doc.reference("references" => attribute.references,
-                        "referenced-object" => attribute.referenced_object.qualified_name,
-                        "polymorphic" => attribute.polymorphic?.to_s,
-                        "link-name" => attribute.referencing_link_name,
-                        "inverse-multiplicity" => attribute.inverse_multiplicity.to_s,
-                        "inverse-traversable" => attribute.inverse_traversable?.to_s,
-                        "inverse-relationship" => attribute.inverse_relationship_name.to_s)
-        end
-
-        if attribute.persistent?
-          doc.persistent(collect_attributes(attribute.sql, %w(column_name sql_type identity? sparse?
-                                                              calculation)))
-        end
-
-      end
-    end
-
-    def visit_table(table)
-      table_attributes = %w(table_name qualified_table_name)
-      doc.table(collect_attributes(table, table_attributes)) do
-        constraint_attributes = %w(name constraint_name qualified_constraint_name invariant?)
-        tag_each(table, :constraints) do |constraint|
-          doc.tag!("sql-constraint", collect_attributes(constraint, constraint_attributes))
-        end
-
-        tag_each(table, :function_constraints) do |constraint|
-          doc.tag!("function-constraint",
-                   collect_attributes(constraint, constraint_attributes)) do
-            tag_each(constraint, :parameters) do |parameter|
-              doc.parameter(:name => parameter)
+        def visit_table(table)
+          table_attributes = %w(  table_name qualified_table_name  )
+          doc.table(collect_attributes(table, table_attributes)) do
+            constraint_attributes = %w(  name constraint_name qualified_constraint_name invariant?  )
+            tag_each(table, :constraints) do |constraint|
+              doc.tag!("sql-constraint", collect_attributes(constraint, constraint_attributes))
             end
+
+            tag_each(table, :function_constraints) do |constraint|
+              doc.tag!("function-constraint",
+                       collect_attributes(constraint, constraint_attributes)) do
+                tag_each(constraint, :parameters) do |parameter|
+                  doc.parameter(:name => parameter)
+                end
+              end
+            end
+
+            tag_each(table, :validations) do |validation|
+              doc.validation(:name => validation.name)
+            end
+
+            tag_each(table, :triggers) do |trigger|
+              doc.trigger(collect_attributes(trigger, %w(  name qualified_trigger_name  ))) do
+                tag_each(trigger, :after) do |after|
+                  doc.after(:condition => after)
+                end
+                tag_each(trigger, :instead_of) do |instead_of|
+                  doc.tag!("instead-of", :condition => instead_of)
+                end
+              end
+            end
+
+            index_attributes = %w(  filter name cluster? unique?  )
+            tag_each(table, :indexes) do |index|
+              doc.index(collect_attributes(index, index_attributes)) do
+                tag_each(index, :attribute_names) do |attribute|
+                  doc.column(:name => attribute)
+                end
+
+                tag_each(index, :include_attribute_names) do |attribute|
+                  doc.column(:name => attribute)
+                end
+              end
+            end
+
+            key_attributes = %w(  name referenced_object_type_name on_update on_delete constraint_name  )
+            tag_each(table, :foreign_keys) do |key|
+              doc.tag!("foreign-key", {:table => table.table_name}, collect_attributes(key, key_attributes)) do
+                doc.tag!("referencing-columns") do
+                  key.attribute_names.zip(key.referenced_attribute_names) do |attribute, referenced|
+                    doc.column(:from => attribute, :to => referenced)
+                  end
+                end
+              end
+            end
+
           end
         end
 
-        tag_each(table, :validations) do |validation|
-          doc.validation(:name => validation.name)
-        end
-
-        tag_each(table, :triggers) do |trigger|
-          doc.trigger(collect_attributes(trigger, %w(name qualified_trigger_name))) do
-            tag_each(trigger, :after) do |after|
-              doc.after(:condition => after)
-            end
-            tag_each(trigger, :instead_of) do |instead_of|
-              doc.tag!("instead-of", :condition => instead_of)
-            end
-          end
-        end
-
-        index_attributes = %w(filter name cluster? unique?)
-        tag_each(table, :indexes) do |index|
-          doc.index(collect_attributes(index, index_attributes)) do
-            tag_each(index, :attribute_names) do |attribute|
-              doc.column(:name => attribute)
-            end
-
-            tag_each(index, :include_attribute_names) do |attribute|
-              doc.column(:name => attribute)
-            end
-          end
-        end
-
-        key_attributes = %w(name referenced_object_type_name on_update on_delete constraint_name)
-        tag_each(table, :foreign_keys) do |key|
-          doc.tag!("foreign-key", {:table => table.table_name}, collect_attributes(key, key_attributes)) do
-            doc.tag!("referencing-columns") do
-              key.attribute_names.zip(key.referenced_attribute_names) do |attribute, referenced|
-                doc.column(:from => attribute, :to => referenced)
+        def add_tags(item)
+          unless item.tags.empty?
+            doc.tag!("tags") do
+              item.tags.each_pair do |tag, value|
+                doc.tag!(tag) { format_text(value) }
               end
             end
           end
         end
-        
-      end
-    end
 
-    def add_tags(item)
-      unless item.tags.empty?
-        doc.tag!("tags") do
-          item.tags.each_pair do |tag, value|
-            doc.tag!(tag) { format_text(value) }
-          end
+        def attribute_ref(object_type, name)
+          doc.attribute(:class => object_type.qualified_name, :attribute => name)
         end
+
       end
     end
-
-    def attribute_ref(object_type, name)
-      doc.attribute(:class => object_type.qualified_name, :attribute => name)
-    end
-
-  end
   end
 end
