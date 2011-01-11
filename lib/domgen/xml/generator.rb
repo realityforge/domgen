8570b285c5037f542c8d4d0000307c3a5e6c39fd
Peter Donald <peter@realityforge.org>
2011-01-12 09:30:45 +1100

    Rename docbook generator to xml as that is what it is generating

--- lib/domgen/xml/generator.rb
+++ lib/domgen/xml/generator.rb
@@ -1,6 +1,6 @@
 module Domgen
   module Generator
-    def self.define_docbook_templates
+    def self.define_xml_templates
       [XmlTemplate.new(:repository, Domgen::Xml::Templates::Xml, '#{repository.name}.xml', [Domgen::Xml::Helper])]
     end
   end
