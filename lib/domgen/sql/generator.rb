a795e5cca019d3531fb5eb4d84415c6a30c400d5
Peter Donald <peter@realityforge.org>
2011-01-25 15:56:46 +1100

    Rename template

--- lib/domgen/sql/generator.rb
+++ lib/domgen/sql/generator.rb
@@ -5,7 +5,7 @@ module Domgen
       [
         Template.new(:data_module, "#{template_dir}/ddl.erb", '#{data_module.name}/schema.sql'),
         Template.new(:data_module, "#{template_dir}/constraints.erb", '#{data_module.name}/schema_constraints.sql'),
-        Template.new(:data_module, "#{template_dir}/constraints_check.erb", '#{data_module.name}/finalize/check_constraints.sql'),
+        Template.new(:data_module, "#{template_dir}/finalize.erb", '#{data_module.name}/finalize/schema_finalize.sql'),
       ]
     end
   end
