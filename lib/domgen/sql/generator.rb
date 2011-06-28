9c6cf65bd6e639402a6c33752e728616410da3c2
Peter Donald <peter@realityforge.org>
2011-06-28 21:14:28 +1000

    * Merge the constraints and ddl sql templates * rename templates to reflect that they are mssql specific

--- lib/domgen/sql/generator.rb
+++ lib/domgen/sql/generator.rb
@@ -4,8 +4,7 @@ module Domgen
       template_dir = "#{File.dirname(__FILE__)}/templates"
       [
         Template.new(:data_module, "#{template_dir}/mssql_ddl.erb", '#{data_module.name}/schema.sql'),
-        Template.new(:data_module, "#{template_dir}/constraints.erb", '#{data_module.name}/schema_constraints.sql'),
-        Template.new(:data_module, "#{template_dir}/finalize.erb", '#{data_module.name}/finalize/schema_finalize.sql'),
+        Template.new(:data_module, "#{template_dir}/mssql_finalize.erb", '#{data_module.name}/finalize/schema_finalize.sql'),
       ]
     end
   end
