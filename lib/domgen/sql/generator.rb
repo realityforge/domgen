2d8ced16c4f5c773c2b3bb13975128143634d1fb
Peter Donald <peter@realityforge.org>
2011-06-28 20:52:37 +1000

    Rename ddl template to reflect the fact that is MS SQL specific

--- lib/domgen/sql/generator.rb
+++ lib/domgen/sql/generator.rb
@@ -3,7 +3,7 @@ module Domgen
     def self.define_sql_templates
       template_dir = "#{File.dirname(__FILE__)}/templates"
       [
-        Template.new(:data_module, "#{template_dir}/ddl.erb", '#{data_module.name}/schema.sql'),
+        Template.new(:data_module, "#{template_dir}/mssql_ddl.erb", '#{data_module.name}/schema.sql'),
         Template.new(:data_module, "#{template_dir}/constraints.erb", '#{data_module.name}/schema_constraints.sql'),
         Template.new(:data_module, "#{template_dir}/finalize.erb", '#{data_module.name}/finalize/schema_finalize.sql'),
       ]
