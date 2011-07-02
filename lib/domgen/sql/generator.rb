6657fa9f121696526fd59321dfc6e180a3ba0598
Peter Donald <peter@realityforge.org>
2011-07-02 12:06:21 +1000

    Add in finalize actions for postgres generator

--- lib/domgen/sql/generator.rb
+++ lib/domgen/sql/generator.rb
@@ -11,7 +11,8 @@ module Domgen
     def self.define_pgsql_templates
       template_dir = "#{File.dirname(__FILE__)}/templates"
       [
-        Template.new(:data_module, "#{template_dir}/pgsql_ddl.erb", '#{data_module.name}/schema.sql')
+        Template.new(:data_module, "#{template_dir}/pgsql_ddl.erb", '#{data_module.name}/schema.sql'),
+      Template.new(:data_module, "#{template_dir}/pgsql_finalize.erb", '#{data_module.name}/finalize/schema_finalize.sql'),
       ]
     end
   end
