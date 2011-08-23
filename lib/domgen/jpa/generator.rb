a9e7ad1f6a80996cb7d00b713b0c598b14731c5a
Peter Donald <peter@realityforge.org>
2011-08-23 10:04:50 +1000

    Remove code for generating DAOs and persistence.xml for non-container code. Unlikely to ever need it in practice.

--- lib/domgen/jpa/generator.rb
+++ lib/domgen/jpa/generator.rb
@@ -32,24 +32,5 @@ module Domgen
                      'object_type.jpa.persistent?')
       ]
     end
-
-    def self.define_jpa_dao_templates
-      [
-        Template.new(JPA::FACETS,
-                     :object_type,
-                     "#{JPA::TEMPLATE_DIRECTORY}/dao.erb",
-                     'java/#{object_type.jpa.qualified_dao_name.gsub(".","/")}.java',
-                     [],
-                     'object_type.jpa.persistent?'),
-        Template.new(JPA::FACETS,
-                     :data_module,
-                     "#{JPA::TEMPLATE_DIRECTORY}/entity_manager.erb",
-                     'java/#{data_module.java.package.gsub(".","/")}/dao/SchemaEntityManager.java'),
-      ]
-    end
-
-    def self.define_jpa_jta_persistence_templates
-      [Template.new(JPA::FACETS, :repository, "#{JPA::TEMPLATE_DIRECTORY}/persistence.erb", 'resources/META-INF/persistence.xml')]
-    end
   end
 end
