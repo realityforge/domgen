2c15db9e28845071118558303c1c318782e565c4
Peter Donald <peter@realityforge.org>
2011-08-23 16:26:29 +1000

    Make it possible to configure the log verbosity on a per task basis

--- lib/domgen/rake_tasks.rb
+++ lib/domgen/rake_tasks.rb
@@ -4,6 +4,7 @@ module Domgen
     attr_accessor :description
     attr_accessor :namespace_key
     attr_accessor :filter
+    attr_writer :verbose
 
     attr_reader :repository_key
     attr_reader :key
@@ -23,18 +24,26 @@ module Domgen
 
     private
 
+    def verbose?
+      !!@verbose
+    end
+
     def define
       desc self.description || "Generates the #{key} artifacts."
       namespace self.namespace_key do
         t = task self.key => ["#{self.namespace_key}:load"] do
+          old_level = Domgen::Logger.level
           begin
             FileUtils.rm_rf(self.target_dir) if self.clobber_dir
+            Domgen::Logger.level = verbose? ? ::Logger::DEBUG : ::Logger::WARN
             Domgen.generate(self.repository_key, self.target_dir, self.generator_keys, self.filter)
           rescue Exception => e
             print "An error occurred invoking the generator\n"
             puts $!
             puts $@
             raise e
+          ensure
+            Domgen::Logger.level = old_level
           end
         end
         @task_name = t.name
@@ -45,6 +54,7 @@ module Domgen
   class LoadSchema
     attr_accessor :description
     attr_accessor :namespace_key
+    attr_writer :verbose
 
     attr_reader :filename
 
@@ -57,18 +67,25 @@ module Domgen
 
     private
 
+    def verbose?
+      !!@verbose
+    end
+
     def define
       desc self.description
       namespace self.namespace_key do
         task :load => [self.filename] do
+          old_level = Domgen::Logger.level
           begin
-            #Domgen::Logger.level = Logger::INFO
+            Domgen::Logger.level = verbose? ? ::Logger::DEBUG : ::Logger::WARN
             require self.filename
           rescue Exception => e
             print "An error occurred loading respository\n"
             puts $!
             puts $@
             raise e
+          ensure
+            Domgen::Logger.level = old_level
           end
         end
       end
