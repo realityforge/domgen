be35b86e72db6c981c75d0d01d685b269cfe2ea1
Peter Donald <peter@realityforge.org>
2011-08-23 13:25:43 +1000

    Refactor the gwt facet so that a module is assumed per application

--- lib/domgen/gwt/model.rb
+++ lib/domgen/gwt/model.rb
@@ -14,7 +14,7 @@ module Domgen
       end
 
       def qualified_service_name
-        "#{service.data_module.gwt.shared_package}.#{service_name}"
+        "#{service.data_module.repository.gwt.shared_package}.#{service_name}"
       end
 
       def async_service_name
@@ -22,7 +22,7 @@ module Domgen
       end
 
       def qualified_async_service_name
-        "#{service.data_module.gwt.shared_package}.#{async_service_name}"
+        "#{service.data_module.repository.gwt.shared_package}.#{async_service_name}"
       end
 
       def servlet_name
@@ -30,7 +30,7 @@ module Domgen
       end
 
       def qualified_servlet_name
-        "#{service.data_module.gwt.server_package}.#{servlet_name}"
+        "#{service.data_module.repository.gwt.server_package}.#{servlet_name}"
       end
     end
 
@@ -42,25 +42,45 @@ module Domgen
       end
     end
 
-    class GwtPackage < Domgen.ParentedElement(:data_module)
-      attr_writer :shared_package
-
-      def shared_package
-        @shared_package || "#{data_module.java.package}.shared"
-      end
-
-      attr_writer :server_package
-
-      def server_package
-        @server_package || "#{data_module.java.package}.server"
-      end
-    end
-
     class GwtModule < Domgen.ParentedElement(:repository)
       attr_writer :module_name
 
       def module_name
         @module_name || repository.name
+      end
+
+      attr_writer :package
+
+      def package
+        @package || repository.java.package
+      end
+
+      attr_writer :shared_package
+
+      def shared_package
+        @shared_package || "#{package}.shared"
+      end
+
+      attr_writer :client_package
+
+      def client_package
+        @client_package || "#{package}.client"
+      end
+
+      attr_writer :server_package
+
+      def server_package
+        @server_package || "#{package}.server"
+      end
+
+      attr_writer :gin_module_name
+
+      def gin_module_name
+        @gin_module_name || "#{module_name}ServicesGinModule"
+      end
+
+      def qualified_gin_module_name
+        "#{client_package}.#{gin_module_name}"
       end
     end
   end
@@ -68,6 +88,5 @@ module Domgen
   FacetManager.define_facet(:gwt,
                             Service => Domgen::GWT::GwtService,
                             Method => Domgen::GWT::GwtMethod,
-                            DataModule => Domgen::GWT::GwtPackage,
                             Repository => Domgen::GWT::GwtModule)
 end
