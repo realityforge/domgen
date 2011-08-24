985e3e3f4c76ec46a66d0a46e1871f1fb7e475c5
Peter Donald <peter@realityforge.org>
2011-08-24 14:58:10 +1000

    Improve camelize and add an underscore method. Both derived from rails-ish code

--- lib/domgen/naming.rb
+++ lib/domgen/naming.rb
@@ -1,8 +1,21 @@
 module Domgen
   module Naming
-    def self.camelize(string)
-      return string if string =~ /^[A-Z0-9_]+$/
-      string[0...1].downcase + string[1..100000]
+    def self.camelize(lower_case_and_underscored_word, first_letter_in_uppercase = false)
+      if first_letter_in_uppercase
+        lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+      else
+        lower_case_and_underscored_word.to_s[0].chr.downcase + camelize(lower_case_and_underscored_word, true)[1..-1]
+      end
+    end
+
+    def self.underscore(camel_cased_word)
+      word = camel_cased_word.to_s.dup
+      word.gsub!(/::/, '/')
+      word.gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+      word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+      word.tr!("-", "_")
+      word.downcase!
+      word
     end
   end
 end
