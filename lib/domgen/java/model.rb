6db53220d72074a3d8e4b974aecc08c9fa86ff33
Peter Donald <peter@realityforge.org>
2011-08-27 11:15:58 +1000

    Remove remnants of java facet now that it is no lover used

--- lib/domgen/java/model.rb
+++ lib/domgen/java/model.rb
@@ -1,8 +1,5 @@
 module Domgen
   module Java
-    DEFAULT_ENTITY_PACKAGE_SUFFIX = "entity"
-    DEFAULT_SERVICE_PACKAGE_SUFFIX = "service"
-
     TYPE_MAP = {"string" => "java.lang.String",
                 "integer" => "java.lang.Integer",
                 "boolean" => "java.lang.Boolean",
@@ -11,27 +8,6 @@ module Domgen
                 "i_enum" => "java.lang.Integer",
                 "s_enum" => "java.lang.String",
                 "List" => "java.util.List"}
-
-    class JavaClass < Domgen.ParentedElement(:object_type)
-      attr_writer :classname
-      attr_accessor :label_attribute
-
-      def classname
-        @classname || object_type.name
-      end
-    end
-
-    class JavaEntity < JavaClass
-      attr_writer :debug_attributes
-
-      def debug_attributes
-        @debug_attributes || object_type.attributes.collect { |a| a.name }
-      end
-
-      def qualified_name
-        "#{object_type.data_module.java.entity_package}.#{self.classname}"
-      end
-    end
 
     module JavaCharacteristic
       attr_writer :java_type
@@ -72,142 +48,5 @@ module Domgen
         raise "object_type_to_classname unimplemented"
       end
     end
-
-    class JavaField < Domgen.ParentedElement(:attribute)
-      attr_writer :field_name
-
-      def field_name
-        @field_name || attribute.name
-      end
-
-      include JavaCharacteristic
-
-      protected
-
-      def characteristic
-        attribute
-      end
-
-      def object_type_to_classname(object_type)
-        object_type.java.qualified_name
-      end
-    end
-
-    class JavaService < JavaClass
-      def qualified_name
-        "#{object_type.data_module.java.service_package}.#{self.classname}"
-      end
-    end
-
-    class JavaMethod < Domgen.ParentedElement(:service)
-      def name
-        Domgen::Naming.camelize(service.name.to_s)
-      end
-    end
-
-    class JavaParameter < Domgen.ParentedElement(:parameter)
-      def name
-        Domgen::Naming.camelize(parameter.name.to_s)
-      end
-
-      include JavaCharacteristic
-
-      protected
-
-      def characteristic
-        parameter
-      end
-
-      def object_type_to_classname(object_type)
-        object_type.java.qualified_name
-      end
-    end
-
-    class JavaMethodParameter < JavaParameter
-    end
-
-    class JavaMessageParameter < JavaParameter
-    end
-
-    class JavaReturn < Domgen.ParentedElement(:result)
-
-      include JavaCharacteristic
-
-      protected
-
-      def characteristic
-        result
-      end
-
-      def object_type_to_classname(object_type)
-        object_type.java.qualified_name
-      end
-    end
-
-    class JavaException < Domgen.ParentedElement(:exception)
-      def name
-        exception.name.to_s =~ /Exception$/ ? exception.name.to_s : "#{exception.name}Exception"
-      end
-    end
-
-    class JavaPackage < Domgen.ParentedElement(:data_module)
-      attr_writer :package
-
-      def package
-        @package || "#{data_module.repository.java.package}.#{Domgen::Naming.underscore(data_module.name)}"
-      end
-
-      def package_defined?
-        !@package.nil?
-      end
-
-      attr_writer :entity_package
-
-      def entity_package
-        return @entity_package if @entity_package
-        return "#{package}.#{DEFAULT_ENTITY_PACKAGE_SUFFIX}" if package_defined?
-        "#{data_module.repository.java.entity_package}.#{Domgen::Naming.underscore(data_module.name)}"
-      end
-
-      attr_writer :service_package
-
-      def service_package
-        return @service_package if @service_package
-        return "#{package}.#{DEFAULT_SERVICE_PACKAGE_SUFFIX}" if package_defined?
-        "#{data_module.repository.java.service_package}.#{Domgen::Naming.underscore(data_module.name)}"
-      end
-    end
-
-    class JavaModule < Domgen.ParentedElement(:repository)
-      attr_writer :package
-
-      def package
-        @package || Domgen::Naming.underscore(repository.name)
-      end
-
-      attr_writer :entity_package
-
-      def entity_package
-        @entity_package || "#{package}.#{DEFAULT_ENTITY_PACKAGE_SUFFIX}"
-      end
-
-      attr_writer :service_package
-
-      def service_package
-        @service_package || "#{package}.#{DEFAULT_SERVICE_PACKAGE_SUFFIX}"
-      end
-    end
   end
-
-  FacetManager.define_facet(:java,
-                            Attribute => Domgen::Java::JavaField,
-                            ObjectType => Domgen::Java::JavaEntity,
-                            MessageParameter => Domgen::Java::JavaMessageParameter,
-                            Service => Domgen::Java::JavaService,
-                            Method => Domgen::Java::JavaMethod,
-                            Parameter => Domgen::Java::JavaMethodParameter,
-                            Exception => Domgen::Java::JavaException,
-                            Result => Domgen::Java::JavaReturn,
-                            DataModule => Domgen::Java::JavaPackage,
-                            Repository => Domgen::Java::JavaModule)
 end
