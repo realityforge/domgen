88b78ce37cd578d754afc313257ceaadd558f27a
Peter Donald <peter@realityforge.org>
2011-11-03 11:38:02 +1100

    Start to refactor the java characteristic mixin to support multiple modalities

--- lib/domgen/java/model.rb
+++ lib/domgen/java/model.rb
@@ -8,14 +8,14 @@ module Domgen
     module JavaCharacteristic
       attr_writer :java_type
 
-      def java_type
+      def java_type(modality = :default)
         return @java_type if @java_type
-        return primitive_java_type if primitive?
-        non_primitive_java_type
+        return primitive_java_type if primitive?(modality)
+        non_primitive_java_type(modality)
       end
 
-      def non_primitive_java_type
-        if :reference == characteristic.characteristic_type
+      def non_primitive_java_type(modality = :default)
+        if characteristic.reference?
           entity_to_classname(characteristic.referenced_entity)
         elsif characteristic.enum?
           return enumeration_to_classname(characteristic.enumeration)
@@ -24,28 +24,43 @@ module Domgen
         end
       end
 
-      def primitive?
-        (characteristic.characteristic_type == :integer || characteristic.characteristic_type == :boolean) && !characteristic.nullable? && (!characteristic.respond_to?(:generated_value?) || !characteristic.generated_value?)
+      def primitive?(modality = :default)
+        return false if characteristic.nullable?
+        return false if (characteristic.respond_to?(:generated_value?) && characteristic.generated_value?)
+        return true if characteristic.integer? || characteristic.boolean?
+
+        if :default == modality
+          return false
+        elsif :transport == modality
+          return false unless attribute.reference?
+          return characteristic.referenced_entity.primary_key.integer? || characteristic.referenced_entity.primary_key.boolean?
+        else
+          error("unknown modality #{modality}")
+        end
       end
 
       def primitive_java_type
-        return "int" if :integer == characteristic.characteristic_type
-        return "boolean" if :boolean == characteristic.characteristic_type
+        return "int" if characteristic.integer?
+        return "boolean" if characteristic.boolean?
         error("primitive_java_type invoked for non primitive method")
       end
 
       protected
 
+      def entity_to_classname(entity)
+        entity.send(facet_key).qualified_name
+      end
+
+      def enumeration_to_classname(enumeration)
+        enumeration.send(facet_key).qualified_name
+      end
+
       def characteristic
         raise "characteristic unimplemented"
       end
 
-      def entity_to_classname(entity)
-        raise "entity_to_classname unimplemented"
-      end
-
-      def enumeration_to_classname(enumeration)
-        raise "enumeration_to_classname unimplemented"
+      def facet_key
+        raise "facet_key unimplemented"
       end
     end
 
@@ -54,12 +69,8 @@ module Domgen
 
       protected
 
-      def entity_to_classname(entity)
-        entity.jpa.qualified_entity_name
-      end
-
-      def enumeration_to_classname(enumeration)
-        enumeration.jpa.qualified_enumeration_name
+      def facet_key
+        :jpa
       end
     end
 
@@ -68,12 +79,8 @@ module Domgen
 
       protected
 
-      def entity_to_classname(entity)
-        entity.imit.qualified_imitation_name
-      end
-
-      def enumeration_to_classname(enumeration)
-        enumeration.imit.qualified_name
+      def facet_key
+        :imit
       end
     end
   end
