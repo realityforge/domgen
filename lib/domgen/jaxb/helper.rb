22c36b723fa1b9b6dc02302b3588c4905265c1d7
Peter Donald <peter@realityforge.org>
2011-11-30 22:57:26 +1100

    Wrap all collection fields in their own element

--- lib/domgen/jaxb/helper.rb
+++ lib/domgen/jaxb/helper.rb
@@ -7,7 +7,14 @@ module Domgen
       end
 
       def jaxb_field_annotation(field)
-        "@javax.xml.bind.annotation.Xml#{field.jaxb.element? ? "Element" : "Attribute" }( name = \"#{field.jaxb.xml_name}\", required = #{field.jaxb.required?}#{namespace_annotation_parameter(field.jaxb) } )"
+        if field.collection?
+<<JAVA
+   @javax.xml.bind.annotation.XmlElementWrapper( name = "#{Domgen::Naming.pluralize(field.jaxb.xml_name)}", required = #{field.jaxb.required?}#{namespace_annotation_parameter(field.jaxb) })
+   @javax.xml.bind.annotation.XmlElement( name = "#{field.jaxb.xml_name}" )
+JAVA
+        else
+          "@javax.xml.bind.annotation.Xml#{field.jaxb.element? ? "Element" : "Attribute" }( name = \"#{field.jaxb.xml_name}\", required = #{field.jaxb.required?}#{namespace_annotation_parameter(field.jaxb) } )"
+        end
       end
     end
   end
