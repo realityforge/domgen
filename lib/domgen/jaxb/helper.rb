6e22ae3ffaec02bb42c3404cc246302f56d5e635
Peter Donald <peter@realityforge.org>
2012-06-05 15:09:29 +1000

    Remove Type suffix from xml type names

--- lib/domgen/jaxb/helper.rb
+++ lib/domgen/jaxb/helper.rb
@@ -11,7 +11,7 @@ module Domgen
         s << "@javax.xml.bind.annotation.XmlAccessorType( javax.xml.bind.annotation.XmlAccessType.FIELD )\n"
         ns = namespace_annotation_parameter(struct.xml)
         s << "@javax.xml.bind.annotation.XmlRootElement( name = \"#{struct.xml.name}\"#{ns} )\n" if struct.top_level?
-        s << "@javax.xml.bind.annotation.XmlType( name = \"#{struct.name}Type\", propOrder = {#{struct.fields.collect{|field| "\"#{field.name}\""}.join(", ")}}#{ns} )\n"
+        s << "@javax.xml.bind.annotation.XmlType( name = \"#{struct.name}\", propOrder = {#{struct.fields.collect{|field| "\"#{field.name}\""}.join(", ")}}#{ns} )\n"
         s
       end
 
