c299fa930891c06a9dd1e0f2358346572fe81c53
Peter Donald <peter@realityforge.org>
2013-10-15 09:13:00 +1100

    Disable autodetection of json serialization via jackson as we are explicit about everything

--- lib/domgen/jackson/helper.rb
+++ lib/domgen/jackson/helper.rb
@@ -17,7 +17,7 @@ module Domgen
     module Helper
       def jackson_class_annotations(struct)
         s = ''
-        s << "@org.codehaus.jackson.annotate.JsonAutoDetect( value = org.codehaus.jackson.annotate.JsonMethod.FIELD )\n"
+        s << "@org.codehaus.jackson.annotate.JsonAutoDetect( value = org.codehaus.jackson.annotate.JsonMethod.NONE )\n"
         s << "@org.codehaus.jackson.annotate.JsonTypeName( \"#{struct.json.name}\" )\n"
         s << "@org.codehaus.jackson.annotate.JsonPropertyOrder({#{struct.fields.collect{|field| "\"#{field.name}\""}.join(", ")}})"
         s
