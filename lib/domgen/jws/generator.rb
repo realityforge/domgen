4a7d82744c94b06079aafdfa638ccaf8aefdadcd
Peter Donald <peter@realityforge.org>
2014-03-12 15:13:37 +1100

    Stop generating wsdls into multiple locations as we only required this due to bugs in our code

--- lib/domgen/jws/generator.rb
+++ lib/domgen/jws/generator.rb
@@ -62,19 +62,6 @@ Domgen.template_set(:jws_wsdl_resources) do |template_set|
 end
 
 Domgen.template_set(:jws_wsdl_assets) do |template_set|
-  # GlassFish 4 / EE4 requires that the files appear in META-INF which seems counter to documentation at
-  # https://jax-ws.java.net/nonav/2.1.5/docs/catalog-support.html
-  template_set.template(Domgen::Generator::JWS::FACETS,
-                        :service,
-                        "#{Domgen::Generator::JWS::TEMPLATE_DIRECTORY}/wsdl.xml.erb",
-                        'main/webapp/META-INF/wsdl/#{service.jws.wsdl_name}',
-                        Domgen::Generator::JWS::HELPERS)
-  template_set.template(Domgen::Generator::JWS::FACETS,
-                        :repository,
-                        "#{Domgen::Generator::JWS::TEMPLATE_DIRECTORY}/jax_ws_catalog.xml.erb",
-                        'main/webapp/META-INF/jax-ws-catalog.xml',
-                        Domgen::Generator::JWS::HELPERS)
-  # GlassFish 3.1.2.2 / EE6 requires that the files appear in WEB-INF
   template_set.template(Domgen::Generator::JWS::FACETS,
                         :service,
                         "#{Domgen::Generator::JWS::TEMPLATE_DIRECTORY}/wsdl.xml.erb",
