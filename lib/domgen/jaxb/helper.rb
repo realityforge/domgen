7030c9c293e883a21f181a7a4aecd7d075afd165
Peter Donald <peter@realityforge.org>
2014-05-22 13:35:31 +1000

    Use names for contained elements raht erhtn type fields

--- lib/domgen/jaxb/helper.rb
+++ lib/domgen/jaxb/helper.rb
@@ -41,8 +41,8 @@ module Domgen
       def jaxb_field_annotation(field, wrap_collections = true)
         if field.collection?
           s = ''
-          s << "@javax.xml.bind.annotation.XmlElementWrapper( name = \"#{field.xml.name}\", required = #{field.xml.required?}, nillable = false#{namespace_annotation_parameter(field.struct.xml)} )\n" if wrap_collections
-          s << "  @javax.xml.bind.annotation.XmlElement( name = \"#{field.xml.component_name}\"#{namespace_annotation_parameter(field.struct.xml)} )\n"
+          s << "@javax.xml.bind.annotation.XmlElementWrapper( name = \"#{field.xml.name}\", required = #{field.xml.required?}, nillable = false )\n" if wrap_collections
+          s << "  @javax.xml.bind.annotation.XmlElement( name = \"#{field.xml.component_name}\" )\n"
           s
         else
           "@javax.xml.bind.annotation.Xml#{field.xml.element? ? "Element" : "Attribute" }( name = \"#{field.xml.name}\", required = #{field.xml.required?} )\n"
