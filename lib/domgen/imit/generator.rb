9fbe147d9921b6999a5b3e333f0c4d5c8d24e684
Peter Donald <peter@realityforge.org>
2014-12-28 10:06:25 +1100

    Restructure the generator namespace, to simplify the way that artifacts can be partitioned

--- lib/domgen/imit/generator.rb
+++ lib/domgen/imit/generator.rb
@@ -17,10 +17,13 @@ module Domgen
     module Imit
       TEMPLATE_DIRECTORY = "#{File.dirname(__FILE__)}/templates"
       HELPERS = [Domgen::Java::Helper]
+      SERVER_HELPERS = [Domgen::JPA::Helper, Domgen::Java::Helper]
       FACETS = [:imit]
+      SERVER_FACETS = [:imit, :jpa]
     end
   end
 end
+
 Domgen.template_set(:imit_metadata) do |template_set|
   template_set.template(Domgen::Generator::Imit::FACETS,
                         :repository,
@@ -29,7 +32,7 @@ Domgen.template_set(:imit_metadata) do |template_set|
                         Domgen::Generator::Imit::HELPERS)
 end
 
-Domgen.template_set(:imit_entity) do |template_set|
+Domgen.template_set(:imit_client_entity) do |template_set|
   template_set.template(Domgen::Generator::Imit::FACETS,
                         :entity,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/client/entity.java.erb",
@@ -74,7 +77,7 @@ Domgen.template_set(:imit_entity) do |template_set|
                         :repository,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/client/router_impl.java.erb",
                         'main/java/#{repository.imit.qualified_client_router_impl_name.gsub(".","/")}.java',
-                        Domgen::Generator::Imit::HELPERS + [Domgen::JPA::Helper, Domgen::Java::Helper])
+                        Domgen::Generator::Imit::HELPERS)
   template_set.template(Domgen::Generator::Imit::FACETS,
                         :repository,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/client/router_interface.java.erb",
@@ -87,7 +90,7 @@ Domgen.template_set(:imit_entity) do |template_set|
                         Domgen::Generator::Imit::HELPERS)
 end
 
-Domgen.template_set(:imit_gwt_proxy) do |template_set|
+Domgen.template_set(:imit_client_service) do |template_set|
   template_set.template(Domgen::Generator::Imit::FACETS,
                         :service,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/client/service.java.erb",
@@ -109,109 +112,112 @@ Domgen.template_set(:imit_gwt_proxy) do |template_set|
                         'main/java/#{exception.imit.qualified_name.gsub(".","/")}.java',
                         Domgen::Generator::Imit::HELPERS)
 end
-Domgen.template_set(:imit_test_module) do |template_set|
-  template_set.template(Domgen::Generator::Imit::FACETS + [:gwt_rpc],
-                        :repository,
-                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/client/mock_services_module.java.erb",
-                        'test/java/#{repository.imit.qualified_mock_services_module_name.gsub(".","/")}.java',
-                        [Domgen::Java::Helper])
+
+%w(main test).each do |type|
+  Domgen.template_set(:"imit_client_#{type}_qa") do |template_set|
+    template_set.template(Domgen::Generator::Imit::FACETS + [:gwt_rpc],
+                          :repository,
+                          "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/client/mock_services_module.java.erb",
+                          type + '/java/#{repository.imit.qualified_mock_services_module_name.gsub(".","/")}.java',
+                          [Domgen::Java::Helper])
+  end
 end
 
-Domgen.template_set(:imit_module) do |template_set|
-  template_set.template(Domgen::Generator::Imit::FACETS + [:gwt_rpc],
-                        :repository,
-                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/client/mock_services_module.java.erb",
-                        'main/java/#{repository.imit.qualified_mock_services_module_name.gsub(".","/")}.java',
-                        [Domgen::Java::Helper])
-end
-
-Domgen.template_set(:imit_jpa_listener) do |template_set|
-  template_set.template(Domgen::Generator::Imit::FACETS + [:jpa],
+Domgen.template_set(:imit_server_entity_listener) do |template_set|
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
                         :repository,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/change_listener.java.erb",
                         'main/java/#{repository.imit.qualified_change_listener_name.gsub(".","/")}.java',
-                        [Domgen::Java::Helper])
-  template_set.template(Domgen::Generator::Imit::FACETS + [:jpa],
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
                         :repository,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/change_recorder.java.erb",
                         'main/java/#{repository.imit.qualified_change_recorder_name.gsub(".","/")}.java',
-                        [Domgen::Java::Helper])
-  template_set.template(Domgen::Generator::Imit::FACETS + [:jpa],
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+end
+
+Domgen.template_set(:imit_server_entity_replication) do |template_set|
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
                         :repository,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/change_recorder_impl.java.erb",
                         'main/java/#{repository.imit.qualified_change_recorder_impl_name.gsub(".","/")}.java',
-                        [Domgen::Java::Helper])
-end
-
-Domgen.template_set(:imit_jpa) do |template_set|
-  facets = Domgen::Generator::Imit::FACETS + [:jpa]
-  helpers = Domgen::Generator::Imit::HELPERS + [Domgen::JPA::Helper, Domgen::Java::Helper]
-  template_set.template(facets,
-                        :repository,
-                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/jpa_encoder.java.erb",
-                        'main/java/#{repository.imit.qualified_jpa_encoder_name.gsub(".","/")}.java',
-                        helpers)
-  template_set.template(facets,
-                        :repository,
-                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/graph_encoder.java.erb",
-                        'main/java/#{repository.imit.qualified_graph_encoder_name.gsub(".","/")}.java',
-                        helpers)
-  template_set.template(facets,
-                        :repository,
-                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/graph_encoder_impl.java.erb",
-                        'main/java/#{repository.imit.qualified_graph_encoder_impl_name.gsub(".","/")}.java',
-                        helpers)
-  template_set.template(facets,
-                        :repository,
-                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/session_context.java.erb",
-                        'main/java/#{repository.imit.qualified_server_session_context_name.gsub(".","/")}.java',
-                        helpers)
-  template_set.template(facets,
-                        :repository,
-                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/replication_interceptor.java.erb",
-                        'main/java/#{repository.imit.qualified_replication_interceptor_name.gsub(".","/")}.java',
-                        helpers)
-  template_set.template(facets,
-                        :repository,
-                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/session.java.erb",
-                        'main/java/#{repository.imit.qualified_session_name.gsub(".","/")}.java',
-                        helpers)
-  template_set.template(facets,
-                        :repository,
-                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/session_manager.java.erb",
-                        'main/java/#{repository.imit.qualified_session_manager_name.gsub(".","/")}.java',
-                        helpers)
-  template_set.template(facets,
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
                         :repository,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/router_interface.java.erb",
                         'main/java/#{repository.imit.qualified_router_interface_name.gsub(".","/")}.java',
-                        helpers)
-  template_set.template(facets,
+                        Domgen::Generator::Imit::HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
                         :repository,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/router_impl.java.erb",
                         'main/java/#{repository.imit.qualified_router_impl_name.gsub(".","/")}.java',
-                        helpers)
-  template_set.template(facets,
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
                         :repository,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/message_generator.java.erb",
                         'main/java/#{repository.imit.qualified_message_generator_name.gsub(".","/")}.java',
-                        helpers)
-  template_set.template(facets,
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
+                        :repository,
+                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/message_generator_interface.java.erb",
+                        'main/java/#{repository.imit.qualified_message_generator_interface_name.gsub(".","/")}.java',
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
                         :repository,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/message_constants.java.erb",
                         'main/java/#{repository.imit.qualified_message_constants_name.gsub(".","/")}.java',
-                        helpers)
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+end
+
+Domgen.template_set(:imit_server_entity => [:imit_server_entity_listener, :imit_server_entity_replication])
+
+Domgen.template_set(:imit_server_service) do |template_set|
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
+                        :repository,
+                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/jpa_encoder.java.erb",
+                        'main/java/#{repository.imit.qualified_jpa_encoder_name.gsub(".","/")}.java',
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
+                        :repository,
+                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/graph_encoder.java.erb",
+                        'main/java/#{repository.imit.qualified_graph_encoder_name.gsub(".","/")}.java',
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
+                        :repository,
+                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/graph_encoder_impl.java.erb",
+                        'main/java/#{repository.imit.qualified_graph_encoder_impl_name.gsub(".","/")}.java',
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
+                        :repository,
+                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/session_context.java.erb",
+                        'main/java/#{repository.imit.qualified_server_session_context_name.gsub(".","/")}.java',
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
+                        :repository,
+                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/replication_interceptor.java.erb",
+                        'main/java/#{repository.imit.qualified_replication_interceptor_name.gsub(".","/")}.java',
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
+                        :repository,
+                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/session.java.erb",
+                        'main/java/#{repository.imit.qualified_session_name.gsub(".","/")}.java',
+                        Domgen::Generator::Imit::SERVER_HELPERS)
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
+                        :repository,
+                        "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/session_manager.java.erb",
+                        'main/java/#{repository.imit.qualified_session_manager_name.gsub(".","/")}.java',
+                        Domgen::Generator::Imit::SERVER_HELPERS)
 end
 
 Domgen.template_set(:imit_server_qa) do |template_set|
-  template_set.template(Domgen::Generator::Imit::FACETS + [:jpa],
+  template_set.template(Domgen::Generator::Imit::SERVER_FACETS,
                         :repository,
                         "#{Domgen::Generator::Imit::TEMPLATE_DIRECTORY}/server/net_module.java.erb",
                         'test/java/#{repository.imit.qualified_server_net_module_name.gsub(".","/")}.java',
-                        [Domgen::Java::Helper])
+                        Domgen::Generator::Imit::SERVER_HELPERS)
 end
 
 Domgen.template_set(:imit_shared => [:imit_metadata])
-Domgen.template_set(:imit_server => [:imit_jpa, :imit_jpa_listener, :imit_server_qa])
-Domgen.template_set(:imit_client => [:imit_test_module, :imit_gwt_proxy, :imit_entity])
+Domgen.template_set(:imit_server => [:imit_server_service, :imit_server_entity, :imit_server_qa])
+Domgen.template_set(:imit_client => [:imit_client_test_qa, :imit_client_service, :imit_client_entity])
 Domgen.template_set(:imit => [:imit_client, :imit_server, :imit_shared])
