b8f11adb6eeb84f07796e7db831cf64e4a6c514e
Peter Donald <peter@realityforge.org>
2015-01-26 15:52:23 +1100

    Add a schema set for public xsds

--- lib/domgen/xml/generator.rb
+++ lib/domgen/xml/generator.rb
@@ -40,6 +40,15 @@ Domgen.template_set(:xml_xsd_resources) do |template_set|
                         :name => 'META-INF/schema.xsd')
 end
 
+Domgen.template_set(:xml_public_xsd_webapp) do |template_set|
+  template_set.template(Domgen::Generator::Xml::FACETS,
+                        :data_module,
+                        "#{Domgen::Generator::Xml::TEMPLATE_DIRECTORY}/schema.xsd.erb",
+                        'main/webapp/formats/#{data_module.repository.name}/#{data_module.xml.xsd_name}',
+                        Domgen::Generator::Xml::HELPERS,
+                        :name => 'formats/schema.xsd')
+end
+
 Domgen.template_set(:xml_doc) do |template_set|
   template_set.xml_template([],
                             :repository,
