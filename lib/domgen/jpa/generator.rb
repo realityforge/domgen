e4b7c5cb6c3e2b813228f003d6fbbfd204fa182b
Peter Donald <peter@realityforge.org>
2015-09-03 10:48:07 +1000

    Make it possible to generate orm.xml in test hierarchy

--- lib/domgen/jpa/generator.rb
+++ lib/domgen/jpa/generator.rb
@@ -135,4 +135,11 @@ Domgen.template_set(:jpa_test_persistence_xml) do |template_set|
                         'test/resources/META-INF/persistence.xml')
 end
 
+Domgen.template_set(:jpa_test_orm_xml) do |template_set|
+  template_set.template(Domgen::Generator::JPA::FACETS,
+                        :repository,
+                        "#{Domgen::Generator::JPA::TEMPLATE_DIRECTORY}/orm.xml.erb",
+                        'test/resources/META-INF/orm.xml')
+end
+
 Domgen.template_set(:jpa => [:jpa_orm_xml, :jpa_persistence_xml, :jpa_model])
