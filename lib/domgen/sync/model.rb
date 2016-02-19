e28ab13068548ea50108ac0c434ce9b5fe001341
Peter Donald <peter@realityforge.org>
2016-02-19 11:41:40 +1100

    Fix the update_via_sync? method to actually work

--- lib/domgen/sync/model.rb
+++ lib/domgen/sync/model.rb
@@ -368,10 +368,11 @@ module Domgen
 
       def update_via_sync?
         entity.attributes.select do |a|
-          a.sync? && !a.primary_key? &&
-          (!entity.sync.transaction_time? || ![:CreatedAt, :DeletedAt].include?(a.name)) &&
-            !(a.reference? && !a.referenced_entity.sync.master?)
-        end
+          a.sync? &&
+            !a.primary_key? &&
+            ![:MasterSynchronized, :CreatedAt, :DeletedAt].include?(a.name) &&
+            !(a.reference? && a.referenced_entity.sync? && a.referenced_entity.sync.synchronize?)
+        end.size > 0
       end
 
       def master_data_module
