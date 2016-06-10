63f987861dc8ab02830aa65bef723990725153fd
Peter Donald <peter@realityforge.org>
2016-06-10 16:38:37 +1000

    Whitespace

--- lib/domgen/jms/model.rb
+++ lib/domgen/jms/model.rb
@@ -36,17 +36,17 @@ module Domgen
       end
 
       def endpoint_methods
-        repository.data_modules.select{|data_module| data_module.jms?}.collect do |data_module|
-          data_module.services.select{|service| service.jms?}.collect do |service|
-            service.methods.select{|method| method.jms? && method.jms.mdb?}
+        repository.data_modules.select { |data_module| data_module.jms? }.collect do |data_module|
+          data_module.services.select { |service| service.jms? }.collect do |service|
+            service.methods.select { |method| method.jms? && method.jms.mdb? }
           end
         end.flatten
       end
 
       def router_methods
-        repository.data_modules.select{|data_module| data_module.jms?}.collect do |data_module|
-          data_module.services.select{|service| service.jms?}.collect do |service|
-            service.methods.select{|method| method.jms? && method.jms.router?}
+        repository.data_modules.select { |data_module| data_module.jms? }.collect do |data_module|
+          data_module.services.select { |service| service.jms? }.collect do |service|
+            service.methods.select { |method| method.jms? && method.jms.router? }
           end
         end.flatten
       end
@@ -64,7 +64,7 @@ module Domgen
       java_artifact :abstract_router, :service, :server, :jms, 'Abstract#{service.ejb.service_name}Impl'
 
       def router?
-        service.methods.any?{|m|m.jms.router?}
+        service.methods.any? { |m| m.jms.router? }
       end
     end
 
@@ -113,7 +113,7 @@ module Domgen
       end
 
       def physical_resource_name
-        @physical_resource_name || destination_resource_name.gsub(/.*\/jms\//,'')
+        @physical_resource_name || destination_resource_name.gsub(/.*\/jms\//, '')
       end
 
       def destination_type=(destination_type)
@@ -180,7 +180,7 @@ module Domgen
       end
 
       def route_to_physical_resource_name
-        @route_to_physical_resource_name || route_to_destination_resource_name.gsub(/.*\/jms\//,'')
+        @route_to_physical_resource_name || route_to_destination_resource_name.gsub(/.*\/jms\//, '')
       end
 
       def valid_destination_types
@@ -195,11 +195,11 @@ module Domgen
 
         if self.mdb?
           raise "Method #{method.qualified_name} is marked as a mdb but has a return value" unless method.return_value.return_type == :void
-          raise "Method #{method.qualified_name} is marked as a mdb but has more than 1 parameter. Parameters: #{method.parameters.collect{|p|p.name}.inspect}" if method.parameters.size > 1
+          raise "Method #{method.qualified_name} is marked as a mdb but has more than 1 parameter. Parameters: #{method.parameters.collect { |p| p.name }.inspect}" if method.parameters.size > 1
         end
         if self.router?
           raise "Method #{method.qualified_name} is marked as a router but has a return value" unless method.return_value.return_type == :void
-          raise "Method #{method.qualified_name} is marked as a router but has more than 1 parameter. Parameters: #{method.parameters.collect{|p|p.name}.inspect}" if method.parameters.size > 1
+          raise "Method #{method.qualified_name} is marked as a router but has more than 1 parameter. Parameters: #{method.parameters.collect { |p| p.name }.inspect}" if method.parameters.size > 1
         end
       end
     end
