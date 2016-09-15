96b2152cd0a2994c1506034b8966c1696ca903f3
Peter Donald <peter@realityforge.org>
2016-09-15 16:24:22 +1000

    Simplify the generation to use helper method

--- lib/domgen/generator.rb
+++ lib/domgen/generator.rb
@@ -18,97 +18,18 @@ module Domgen
 
       Logger.debug "Templates to process: #{templates.collect { |t| t.name }.inspect}"
 
+      targets = {}
+      repository.collect_generation_targets(targets)
+
       templates.each do |template|
         Logger.debug "Evaluating template: #{template.name}"
-        if :repository == template.scope
-          if template.applicable?(repository) && (filter.nil? || filter.call(:repository, repository))
-            template.generate(directory, :repository, repository, unprocessed_files)
+        elements = targets[template.scope]
+
+        elements.each do |element|
+          if template.applicable?(element) && (filter.nil? || filter.call(template.scope, element))
+            template.generate(directory, template.scope, element, unprocessed_files)
           end
-        else
-          repository.data_modules.each do |data_module|
-            if :data_module == template.scope
-              if template.applicable?(data_module) && (filter.nil? || filter.call(:data_module, data_module))
-                template.generate(directory, :data_module, data_module, unprocessed_files)
-              end
-            else
-              if :entity == template.scope
-                data_module.entities.each do |entity|
-                  if template.applicable?(entity) && (filter.nil? || filter.call(:entity, entity))
-                    template.generate(directory, :entity, entity, unprocessed_files)
-                  end
-                end
-              end
-
-              if :dao == template.scope
-                data_module.daos.each do |dao|
-                  if template.applicable?(dao) && (filter.nil? || filter.call(:dao, dao))
-                    template.generate(directory, :dao, dao, unprocessed_files)
-                  end
-                end
-              end
-
-              if :query == template.scope
-                data_module.daos.each do |entity|
-                  entity.queries.each do |query|
-                    if template.applicable?(query) && (filter.nil? || filter.call(:query, query))
-                      template.generate(directory, :query, query, unprocessed_files)
-                    end
-                  end
-                end
-              end
-
-              if :struct == template.scope
-                data_module.structs.each do |struct|
-                  if template.applicable?(struct) && (filter.nil? || filter.call(:struct, struct))
-                    template.generate(directory, :struct, struct, unprocessed_files)
-                  end
-                end
-              end
-
-              if :enumeration == template.scope
-                data_module.enumerations.each do |entity|
-                  if template.applicable?(entity) && (filter.nil? || filter.call(:enumeration, entity))
-                    template.generate(directory, :enumeration, entity, unprocessed_files)
-                  end
-                end
-              end
-
-              if :exception == template.scope
-                data_module.exceptions.each do |entity|
-                  if template.applicable?(entity) && (filter.nil? || filter.call(:exception, entity))
-                    template.generate(directory, :exception, entity, unprocessed_files)
-                  end
-                end
-              end
-
-              if :service == template.scope
-                data_module.services.each do |service|
-                  if template.applicable?(service) && (filter.nil? || filter.call(:service, service))
-                    template.generate(directory, :service, service, unprocessed_files)
-                  end
-                end
-              end
-
-              if :method == template.scope
-                data_module.services.each do |service|
-                  service.methods.each do |method|
-                    if template.applicable?(method) && (filter.nil? || filter.call(:method, method))
-                      template.generate(directory, :method, method, unprocessed_files)
-                    end
-                  end
-                end
-              end
-
-              if :message == template.scope
-                data_module.messages.each do |message|
-                  if template.applicable?(message) && (filter.nil? || filter.call(:message, message))
-                    template.generate(directory, :message, message, unprocessed_files)
-                  end
-                end
-              end
-            end
-          end
-        end
+        end if elements
       end
       Logger.info 'Generator completed'
     end
