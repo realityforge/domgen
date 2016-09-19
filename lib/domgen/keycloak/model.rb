c57a4541019725b27d8f95ff32fb13d9f399de50
Peter Donald <peter@realityforge.org>
2016-09-19 11:28:22 +1000

    Add representation for keycloak clients

--- lib/domgen/keycloak/model.rb
+++ lib/domgen/keycloak/model.rb
@@ -13,6 +13,101 @@
 #
 
 module Domgen
+  module Keycloak
+    class Client < Domgen.ParentedElement(:keycloak_repository)
+      def initialize(keycloak_repository, key, options = {}, &block)
+        @key = key
+        super(keycloak_repository, options, &block)
+      end
+
+      attr_reader :key
+
+      attr_writer :name
+
+      def name
+        Domgen::Naming.underscore(key.to_s)
+      end
+
+      attr_writer :root_url
+
+      def root_url
+        @root_url || "http://#{Domgen::Naming.underscore(keycloak_repository.repository.name)}.example.com"
+      end
+
+      attr_writer :base_url
+
+      def base_url
+        @base_url || '/'
+      end
+
+      attr_writer :standard_flow
+
+      def standard_flow?
+        @standard_flow.nil? ? true : !!@standard_flow
+      end
+
+      attr_writer :implicit_flow
+
+      def implicit_flow?
+        @implicit_flow.nil? ? true : !!@implicit_flow
+      end
+
+      attr_writer :consent_required
+
+      def consent_required?
+        @consent_required.nil? ? false : !!@consent_required
+      end
+
+      attr_writer :bearer_only
+
+      def bearer_only?
+        @bearer_only.nil? ? false : !!@bearer_only
+      end
+
+      attr_writer :service_accounts
+
+      def service_accounts?
+        @service_accounts.nil? ? false : !!@service_accounts
+      end
+
+      attr_writer :direct_access_grants
+
+      def direct_access_grants?
+        @direct_access_grants.nil? ? true : !!@direct_access_grants
+      end
+
+      attr_writer :public_client
+
+      def public_client?
+        @public_client.nil? ? true : !!@public_client
+      end
+
+      attr_writer :frontchannel_logout
+
+      def frontchannel_logout?
+        @frontchannel_logout.nil? ? true : !!@frontchannel_logout
+      end
+
+      attr_writer :full_scope_allowed
+
+      def full_scope_allowed?
+        @full_scope_allowed.nil? ? true : !!@full_scope_allowed
+      end
+
+      attr_writer :surrogate_auth_required
+
+      def surrogate_auth_required?
+        @surrogate_auth_required.nil? ? false : !!@surrogate_auth_required
+      end
+
+      attr_writer :redirect_uris
+
+      def redirect_uris
+        @redirect_uris ||= ["#{root_url}/*"]
+      end
+    end
+  end
+
   FacetManager.facet(:keycloak) do |facet|
     facet.enhance(Repository) do
       include Domgen::Java::BaseJavaGenerator
@@ -32,6 +127,30 @@ module Domgen
 
       def jndi_config_base
         @jndi_config_base || "#{Domgen::Naming.underscore(repository.name)}/keycloak"
+      end
+
+      def client_by_key?(key)
+        !!client_map[key.to_s]
+      end
+
+      def client_by_key(key)
+        raise "No keycloak client with key #{key} defined." unless client_map[key.to_s]
+        client_map[key.to_s]
+      end
+
+      def client(key, options = {}, &block)
+        raise "Keycloak client with id #{key} already defined." if client_map[key.to_s]
+        client_map[key.to_s] = Domgen::Keycloak::Client.new(self, key, options, &block)
+      end
+
+      def clients
+        client_map.values
+      end
+
+      private
+
+      def client_map
+        @clients ||= {}
       end
     end
   end
