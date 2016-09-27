25a8082eeca67808106139a5eb0ca5eb7476b5f3
Peter Donald <peter@realityforge.org>
2016-09-27 17:00:05 +1000

    Rename generated filter to have a more specific and descriptive name

--- lib/domgen/timerstatus/model.rb
+++ lib/domgen/timerstatus/model.rb
@@ -18,7 +18,7 @@ module Domgen
       include Domgen::Java::BaseJavaGenerator
       include Domgen::Java::JavaClientServerApplication
 
-      java_artifact :blocking_filter, :filter, :server, :timerstatus, '#{repository.name}BlockingFilter'
+      java_artifact :blocking_filter, :filter, :server, :timerstatus, '#{repository.name}TimerAppBlockingFilter'
       java_artifact :integration_test, :rest, :server, :timerstatus, '#{repository.name}TimerstatusTest'
 
       def additional_timers
