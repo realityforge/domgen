02dc8cb053fa720ac4317b5e46f4f6996f1fcbef
Peter Donald <peter@realityforge.org>
2016-12-01 12:55:53 +1100

    Camelize after pluralization to ensure pluralization rules only need to work with pascal cased names

--- lib/domgen/jpa/helper.rb
+++ lib/domgen/jpa/helper.rb
@@ -66,7 +66,7 @@ module Domgen
         s << gen_relation_annotation(attribute, false)
         s << gen_fetch_mode_if_specified(attribute)
         if attribute.inverse.multiplicity == :many
-          s << "  private java.util.List<#{attribute.entity.jpa.qualified_name}> #{Reality::Naming.pluralize(Reality::Naming.camelize(attribute.inverse.name))};\n"
+          s << "  private java.util.List<#{attribute.entity.jpa.qualified_name}> #{Reality::Naming.camelize(Reality::Naming.pluralize(attribute.inverse.name))};\n"
         else # attribute.inverse.multiplicity == :one || attribute.inverse.multiplicity == :zero_or_one
           s << "  private #{attribute.entity.jpa.qualified_name} #{Reality::Naming.camelize(attribute.inverse.name)};\n"
         end
