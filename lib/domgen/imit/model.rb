475352f7c0ce5c90a41e7172401589115fad6aa3
Peter Donald <peter@realityforge.org>
2017-07-21 11:13:12 +1000

    Enable filter propagation at session manager and remove historic unused methods

--- lib/domgen/imit/model.rb
+++ lib/domgen/imit/model.rb
@@ -1210,37 +1210,6 @@ CONTENT
             m.disable_facet(:gwt) if m.gwt?
           end
 
-          repository.imit.graphs.each do |graph|
-            if graph.filtered? && !graph.filter_parameter.immutable?
-              s.method(:"Update#{graph.name}Subscription") do |m|
-                m.string(:ClientID, 50)
-                m.reference(graph.instance_root) if graph.instance_root?
-                m.parameter(:Filter, graph.filter_parameter.filter_type, filter_options(graph))
-                m.exception(self.invalid_session_exception)
-              end
-              if graph.instance_root?
-                s.method(:"UpdateMultiple#{graph.name}Subscriptions") do |m|
-                  m.string(:ClientID, 50)
-                  options = {}
-                  pk = repository.entity_by_name(graph.instance_root).primary_key
-                  if pk.enumeration?
-                    options[:enumeration] = pk.enumeration
-                    options[:length] = pk.length
-                  end
-                  if pk.text?
-                    options[:length] = pk.length
-                    options[:min_length] = pk.min_length
-                    options[:allow_blank] = pk.allow_blank?
-                  end
-                  options[:collection_type] = :sequence
-                  options[:nullable] = pk.nullable?
-                  m.parameter(pk.name, pk.attribute_type, options)
-                  m.parameter(:Filter, graph.filter_parameter.filter_type, filter_options(graph))
-                  m.exception(self.invalid_session_exception)
-                end
-              end
-            end
-          end
           if self.poll_replicate_mode?
             s.method(:Poll) do |m|
               m.string(:ClientID, 50)
