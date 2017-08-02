f1b502c34aed51324808eafa1f1f26d3c6c41d56
Peter Donald <peter@realityforge.org>
2017-08-02 11:41:03 +1000

    Generate the rest control endpoint for triggering master->core sync process when the sync process is not "standalone"

--- lib/domgen/syncrecord/generator.rb
+++ lib/domgen/syncrecord/generator.rb
@@ -35,7 +35,7 @@ Domgen::Generator.define([:syncrecord],
                               'control_rest_service.java.erb',
                               'main/java/#{repository.syncrecord.qualified_control_rest_service_name.gsub(".","/")}.java',
                               :additional_facets => [:jaxrs],
-                              :guard => 'repository.syncrecord.sync_methods?')
+                              :guard => 'repository.syncrecord.sync_methods? || (repository.sync? && !repository.sync.standalone?)')
   end
 
   g.template_set(:syncrecord_datasources) do |template_set|
