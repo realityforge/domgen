ce87db85cda93c0085c8f5e17128bb989a958de2
Peter Donald <peter@realityforge.org>
2021-03-11 14:26:28 +1100

    Avoid trigger action to support deletes in self-referential tables when removals disabled for entity

--- lib/domgen/sync/model.rb
+++ lib/domgen/sync/model.rb
@@ -572,7 +572,7 @@ module Domgen
 
             # For self referential, non-transaction time entities, we have to set sql.on_delete
             # attribute otherwise sync will fail to remove the entity during synchronization
-            if a.reference? && a.referenced_entity.qualified_name == a.entity.qualified_name && !entity.transaction_time?
+            if a.reference? && a.referenced_entity.qualified_name == a.entity.qualified_name && !entity.transaction_time? && entity.sync.support_remove?
               a.sql.on_delete = :set_null
             end
 
