6c3bba38e047647c28c19fd8596f887586632987
Peter Donald <peter@realityforge.org>
2022-01-13 10:00:38 +1100

    When entities cane be managed by sync or unmanaged, allow an explicit configuration of default status when creating entities in tests

--- lib/domgen/sync/model.rb
+++ lib/domgen/sync/model.rb
@@ -346,6 +346,12 @@ module Domgen
         @support_unmanaged.nil? ? true : !!@support_unmanaged
       end
 
+      attr_writer :test_factory_defaults_to_unmanaged
+
+      def test_factory_defaults_to_unmanaged?
+        support_unmanaged? && (@test_factory_defaults_to_unmanaged.nil? ? true : !!@test_factory_defaults_to_unmanaged)
+      end
+
       def core_entity=(core_entity)
         @core_entity = core_entity
       end
@@ -520,8 +526,8 @@ module Domgen
                               :description => 'Id of the entity from which this entity was synced',
                               '-facets' => [:sync, :arez, :gwt])
         end
-        self.entity.jpa.create_default(:MasterId => 'null') if self.entity.sync? && self.entity.sync.support_unmanaged?
-        self.entity.jpa.create_default(:CreatedAt => 'now()', :DeletedAt => 'null', :MasterId => 'null') if self.entity.transaction_time? && self.entity.sync.support_unmanaged?
+        self.entity.jpa.create_default(:MasterId => 'null') if self.entity.sync? && self.entity.sync.support_unmanaged? && self.entity.sync.test_factory_defaults_to_unmanaged?
+        self.entity.jpa.create_default(:CreatedAt => 'now()', :DeletedAt => 'null', :MasterId => 'null') if self.entity.transaction_time? && self.entity.sync.support_unmanaged? && self.entity.sync.test_factory_defaults_to_unmanaged?
 
         sync_temp_data_module.entity("#{self.entity.sync.entity_prefix}#{self.entity.name}") do |e|
           e.disable_facets_not_in(Domgen::Sync::VALID_SYNC_TEMP_FACETS)
