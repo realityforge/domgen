04947f2ea79b0b44ee2c23a73d852580ec7b92f2
Harry Seigne <harryseigne@gmail.com>
2024-01-17 11:43:07 +1100

    move sql_analysis generated sql into post_sync so that we can override corruption checks

--- lib/domgen/sql_analysis/model.rb
+++ lib/domgen/sql_analysis/model.rb
@@ -78,24 +78,137 @@ module Domgen
       end
     end
 
+    facet.enhance(Entity) do
+      def validation_values
+        @validation_values ||= {}
+      end
+      def validations
+        validation_values.values
+      end
+
+      def validation?(name)
+        !!validation_values[name.to_s]
+      end
+      def validation(name, options = {}, &block)
+        Domgen.error("Validation named #{name} already defined on table #{qualified_table_name}") if validation?(name)
+        validation = Domgen::Sql::Validation.new(self, name, options, &block)
+        validation_values[name.to_s] = validation
+        validation
+      end
+    end
+
     facet.enhance(DataModule) do
+
+      def sql_id_hierarchy_check
+        (@sql_id_hierarchy_check ||= [])
+      end
+
+      def sql_relationships_match_check
+        (@sql_relationships_match_check ||= [])
+      end
+
+      def sql_declared_validations_check
+        (@sql_declared_validations_check ||= [])
+      end
+
       def entities_to_analyze_id_namespace
-        data_module.entities.select(&:sql_analysis?).select {|entity| entity.abstract? && entity.extends.nil?}
+        data_module.entities.select(&:sql_analysis?).select { |entity| entity.abstract? && entity.extends.nil? }
       end
 
       def references_to_analyze
         data_module.entities.
-          select {|entity| entity.sql_analysis? && entity.concrete? && entity.transaction_time?}.
-          collect {|entity| entity.attributes.select {|attribute| attribute.sql_analysis? && attribute.reference? && attribute.referenced_entity.sql_analysis? && attribute.referenced_entity.transaction_time?}}.
+          select { |entity| entity.sql_analysis? && entity.concrete? && entity.transaction_time? }.
+          collect { |entity| entity.attributes.select { |attribute| attribute.sql_analysis? && attribute.reference? && attribute.referenced_entity.sql_analysis? && attribute.referenced_entity.transaction_time? } }.
           flatten
       end
 
       def entities_with_validations
-        data_module.entities.select(&:sql_analysis?).select {|entity| !entity.sql.validations.empty?}
+        data_module.entities.select(&:sql_analysis?).select { |entity| !entity.sql.validations.empty? }
       end
 
       def standard_corruption_checks?
         !entities_to_analyze_id_namespace.empty? || !references_to_analyze.empty? || !entities_with_validations.empty?
+      end
+
+      def post_complete
+        entities = data_module.sql_analysis.entities_to_analyze_id_namespace
+        corruption_check_entity = data_module.repository.sql_analysis.corruption_check_entity
+        unless entities.empty?
+          entities.each do |entity|
+            sql_id_hierarchy_check <<
+              "INSERT INTO #{corruption_check_entity.sql.qualified_table_name}(#{corruption_check_entity.attribute_by_name(:Category).sql.quoted_column_name}, #{corruption_check_entity.attribute_by_name(:Description).sql.quoted_column_name}, #{corruption_check_entity.attribute_by_name(:Sql).sql.quoted_column_name})
+  VALUES ('Id Namespace', 'An Id must not be appear in multiple tables in the #{entity.qualified_name} hierarchy','
+SELECT #{entity.primary_key.sql.quoted_column_name}, COUNT(*) AS IDCount
+ FROM (
+#{entity.concrete_subtypes.select { |entity| entity.sql_analysis? }.collect do |subtype|
+                "SELECT #{entity.primary_key.sql.quoted_column_name}, ''#{subtype.name}'' AS Type FROM #{subtype.sql.qualified_table_name}"
+              end.join("\nUNION\n")}
+ ) f
+ GROUP BY #{entity.primary_key.sql.quoted_column_name}
+ HAVING COUNT(*) > 1')
+GO"
+          end
+        end
+
+        references_to_analyze =  data_module.sql_analysis.references_to_analyze
+        unless references_to_analyze.empty?
+          references_to_analyze.each do |attribute|
+            sql_relationships_match_check <<
+              "INSERT INTO #{corruption_check_entity.sql.qualified_table_name}(#{corruption_check_entity.attribute_by_name(:Category).sql.quoted_column_name},#{corruption_check_entity.attribute_by_name(:Description).sql.quoted_column_name},#{corruption_check_entity.attribute_by_name(:Sql).sql.quoted_column_name})
+  VALUES ('Relationship', 'The attribute #{attribute.qualified_name} must reference an object and the object must not be deleted before the #{attribute.entity.qualified_name} is deleted','
+SELECT PK.DeletedAt
+ FROM
+  #{attribute.entity.sql.qualified_table_name} PK
+ LEFT JOIN #{if attribute.referenced_entity.final?
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    attribute.referenced_entity.sql.qualified_table_name
+             else
+               (
+                 entity_list = attribute.referenced_entity.compatible_concrete_types
+                 entity_list.collect do |subtype|
+                   "SELECT #{subtype.primary_key.sql.quoted_column_name}, DeletedAt FROM #{subtype.sql.qualified_table_name}"
+                 end.join("\nUNION\n")
+               )
+             end
+              } FK ON PK.#{attribute.sql.column_name} = FK.#{attribute.referenced_entity.primary_key.sql.column_name}
+ WHERE
+  PK.#{attribute.sql.column_name} IS NOT NULL AND (FK.#{attribute.entity.primary_key.sql.quoted_column_name} IS NULL OR (PK.DeletedAt IS NULL AND FK.DeletedAt IS NOT NULL) OR (PK.DeletedAt > FK.DeletedAt))
+')
+GO"
+          end
+        end
+
+        entities_with_validations = data_module.sql_analysis.entities_with_validations
+        unless entities_with_validations.empty?
+          entities_with_validations.each do |entity|
+            entity.sql.validations.select{|v| !v.invariant_negative_sql.nil? }.each do |validation|
+              unless entity.sql_analysis.validation?("DeclaredValidationCheck" + validation.name.to_s)
+                sql_declared_validations_check <<
+                  "INSERT INTO #{corruption_check_entity.sql.qualified_table_name}(#{corruption_check_entity.attribute_by_name(:Category).sql.quoted_column_name},#{corruption_check_entity.attribute_by_name(:Description).sql.quoted_column_name},#{corruption_check_entity.attribute_by_name(:CommonTableExpression).sql.quoted_column_name},#{corruption_check_entity.attribute_by_name(:Sql).sql.quoted_column_name})
+  VALUES ('EntityValidation', 'Enforce the validation #{entity.sql.qualified_table_name}.#{validation.name}: #{ (validation.description || '').gsub("'","''")}',#{cte = validation.invariant_common_table_expression; cte.nil? ? "NULL" : "'" + (cte.strip.gsub("'","''") + "\n'")},'#{validation.invariant_negative_sql.gsub("'","''").strip}')
+GO"
+              end
+            end
+          end
+        end
+        data_module.entities.select{|entity|entity.sql?}.each do |entity|
+          entity.attributes.select{|attribute|attribute.sql? && attribute.reference? && :many != attribute.inverse.multiplicity}.each do |attribute|
+            unless entity.sql_analysis.validation?("DeclaredValidationCheckReferenceMultiplicity")
+              sql_declared_validations_check <<
+                "INSERT INTO #{corruption_check_entity.sql.qualified_table_name}(#{corruption_check_entity.attribute_by_name(:Category).sql.quoted_column_name},#{corruption_check_entity.attribute_by_name(:Description).sql.quoted_column_name},#{corruption_check_entity.attribute_by_name(:Sql).sql.quoted_column_name})
+  VALUES ('Reference Multiplicity', 'The reference #{attribute.qualified_name} to #{attribute.referenced_entity.qualified_name} must be a multiplicity of #{attribute.inverse.multiplicity}','
+SELECT O.#{attribute.referenced_entity.primary_key.sql.quoted_column_name} AS #{attribute.referenced_entity.name}#{attribute.referenced_entity.primary_key.sql.column_name}, COUNT(S.#{attribute.referenced_entity.primary_key.sql.quoted_column_name}) AS RelatedCount
+FROM #{attribute.referenced_entity.sql.qualified_table_name} O
+LEFT JOIN #{entity.sql.qualified_table_name} S ON S.#{attribute.sql.quoted_column_name} = O.#{attribute.referenced_entity.primary_key.sql.quoted_column_name}#{if entity.transaction_time?
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         " AND S.DeletedAt IS NULL"
+                                                                                                                                                               end}
+#{if attribute.referenced_entity.transaction_time?
+                                                                                                                                                                       "WHERE O.DeletedAt IS NULL\n"
+  end}GROUP BY O.#{attribute.referenced_entity.primary_key.sql.quoted_column_name}
+HAVING COUNT(S.#{entity.primary_key.sql.quoted_column_name}) #{:one == attribute.inverse.multiplicity ? '<> 1' : '> 1' }')
+GO"
+            end
+          end
+        end
       end
     end
   end
