00d3b905e09d3b4390fec7e8567f55b5735f26a8
Andrea Granados <andreaagranados@gmail.com>
2024-07-05 14:44:18 +1000

    new line

--- lib/domgen/ejb/model.rb
+++ lib/domgen/ejb/model.rb
@@ -140,8 +140,8 @@ module Domgen
     end
 
     facet.enhance(DataModule) do
-      include Domgen::Java::BaseJavaGenerator
       include Domgen::Java::EEClientServerJavaPackage
+
       java_artifact :aggregate_service_test, :test, :server, :ejb, '#{data_module.name}AggregateServiceTest'
     end
 
