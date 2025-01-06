b53d70efc48190f5c7e4d44df9fe72ef73c11779
Peter Donald <peter@realityforge.org>
2025-01-06 11:22:27 +1100

    Start to store action configuration in method model

--- lib/domgen/action/model.rb
+++ lib/domgen/action/model.rb
@@ -115,6 +115,60 @@ module Domgen
       java_artifact :interceptor_impl, :service, :server, :action, '#{method.name}ActionInterceptor'
       java_artifact :action_impl, :service, :server, :action, '#{method.name}Action'
 
+      attr_writer :max_error_count
+
+      def max_error_count
+        @max_error_count.nil? ? 1 : @max_error_count
+      end
+
+      attr_writer :retry_rate
+
+      def retry_rate
+        @retry_rate.nil? ? 0 : @retry_rate
+      end
+
+      attr_writer :store_response_on_success
+
+      def store_response_on_success?
+        @store_response_on_success.nil? ? true : @store_response_on_success
+      end
+
+      attr_writer :generate_message_on_success
+
+      def generate_message_on_success?
+        @generate_message_on_success.nil? ? true : @generate_message_on_success
+      end
+
+      attr_writer :retain_failed_message_duration
+
+      def retain_failed_message_duration
+        @retain_failed_message_duration.nil? ? 1 : @retain_failed_message_duration
+      end
+
+      attr_writer :store_error_message_on_failure
+
+      def store_error_message_on_failure?
+        @store_error_message_on_failure.nil? ? true : @store_error_message_on_failure
+      end
+
+      attr_writer :persist_on_success
+
+      def persist_on_success?
+        @persist_on_success.nil? ? true : @persist_on_success
+      end
+
+      attr_writer :persist_duration
+
+      def persist_duration
+        @persist_duration.nil? ? nil : @persist_duration
+      end
+
+      attr_writer :clear_error_on_success
+
+      def clear_error_on_success?
+        @clear_error_on_success.nil? ? true : @clear_error_on_success
+      end
+
       def pre_pre_complete
         unless self.method.ejb? && self.method.gwt_rpc?
           self.method.disable_facet(:action)
